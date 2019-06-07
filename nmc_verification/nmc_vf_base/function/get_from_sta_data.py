0c3d57836580a998db8d51d735137bda4b52be9a
liucouhua <liucouhua@163.com>
2019-06-07 21:51:51 +0800

    增加了透视层中参数类型

--- nmc_verification/nmc_vf_base/function/get_from_sta_data.py
+++ nmc_verification/nmc_vf_base/function/get_from_sta_data.py
@@ -66,7 +66,32 @@ def sta_between_lat_range(sta,slat,elat):
     sta1 = sta.loc[(sta['lat']>=slat) & (sta['lat']<= elat)]
     return sta1
 
+def sta_between_alt_range(sta,salt,ealt):
+    sta1 = sta.loc[(sta['alt']>=salt) & (sta['alt']<= ealt)]
+    return sta1
+
 def sta_in_grid_xy(sta,grid):
     sta1 = sta_between_lon_range(sta,grid.slon,grid.elon)
     sta2 = sta_between_lat_range(sta1,grid.slat,grid.elat)
     return sta2
+
+def get_by_para_dict(sta,para_dict):
+    '''
+    para_dict 应具备如下样式
+    para_dict = {'level' :[850,700],  列表形式
+                'time' : ['2018010108','2018010208'], 列表形式
+                'dtime':['24h','36h']，列表形式
+                'id':[54511]， 列表形式
+                'lon': [70,140], 闭区间
+                'lat' :[10,60], 闭区间
+                'alt’: [ 0,9999] 闭区间
+    }
+    '''
+    sta1 = sta_in_level_list(sta,para_dict['level'])
+    sta1 = sta_in_time_list(sta1,para_dict['time'])
+    sta1 = sta_in_dtime_list(sta1, para_dict['dtime'])
+    sta1 = sta_in_id_list(sta1, para_dict['id'])
+    sta1 = sta_between_lon_range(sta1,para_dict['lon'][0],para_dict['lon'][1])
+    sta1 = sta_between_lat_range(sta1, para_dict['lat'][0], para_dict['lat'][1])
+    sta1 = sta_between_alt_range(sta1, para_dict['alt'][0], para_dict['alt'][1])
+    return sta1
---
+++ nmc_verification/nmc_vf_base/perspective/para_dealing.py
@@ -0,0 +1,78 @@
+
+class plot_group:
+    picture = "picture"
+    subplot = "subplot"
+    legend = "legend"
+    axis_x = "axis_x"
+
+
+class data_para:
+    def __init__(self):
+        self.level = "fold"
+        self.time = "fold"
+        self.year = "fold"
+        self.month = "fold"
+        self.xun = "fold"
+        self.hou = "fold"
+        self.day = "fold"
+        self.hour = "fold"
+        self.dtime = "fold"
+        self.dhour = "fold"
+        self.dday = "fold"
+        self.id = "fold"
+        self.lon = "fold"
+        self.lat = "fold"
+        self.alt = "fold"
+
+    def set_level_unfold(self):
+        self.level = "unfold"
+
+    def set_time_unfold(self):
+        self.time = "unfold"
+
+    def set_year_unfold(self):
+        self.time = "fold"
+        self.year = "unfold"
+
+    def set_month_unfold(self):
+        self.time = "fold"
+        self.month = "unfold"
+
+    def set_xun_unfold(self):
+        self.time = "fold"
+        self.xun = "unfold"
+
+    def set_hou_unfold(self):
+        self.time = "fold"
+        self.hou = "unfold"
+
+    def set_day_unfold(self):
+        self.time = "fold"
+        self.day = "unfold"
+
+    def set_hour_unfold(self):
+        self.time = "fold"
+        self.hour = "unfold"
+
+    def set_dtime_unfold(self):
+        self.dtime = "unfold"
+
+    def set_dhour_unfold(self):
+        self.dtime = "fold"
+        self.dhour = "unfold"
+
+    def set_dday_unfold(self):
+        self.dtime = "fold"
+        self.dday = "unfold"
+
+    def set_id_unfold(self):
+        self.id = "unfold"
+
+    def set_lon_unfold(self,dlon,slon = 0):
+        self.lon = "unfold"
+        
+    def set_lat_unfold(self,dlat,slat = 0):
+        self.lat = "unfold"
+
+    def set_alt_unfold(self,dalt,alt = 0):
+        self.alt = "unfold"
