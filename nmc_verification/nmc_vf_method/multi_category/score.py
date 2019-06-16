97cfc9ef59ff94f17011f8f0ed40a03c970e8a0d
liucouhua <liucouhua@163.com>
2019-06-16 12:29:18 +0800

    扩展兼容输入是连续变量和类别变量的情形

--- nmc_verification/nmc_vf_method/multi_category/score.py
+++ nmc_verification/nmc_vf_method/multi_category/score.py
@@ -1,10 +1,17 @@
 import numpy as np
 
-def accuracy(ob,fo):
+def accuracy(ob,fo,threshold_list = None):
     #多分类预报准确率
-    #ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-    ob1 = ob.reshape((-1))
-    fo1 = fo.reshape((-1))
+    if threshold_list is None:
+        #ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
+
+        ob1 = ob.reshape((-1))
+        fo1 = fo.reshape((-1))
+    else:
+        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
+        ob1 = None #此处需修改
+        fo1 = None #此处需修改
+
     ob_len = len(ob1)
     true_num = 0
 
@@ -15,14 +22,19 @@ def accuracy(ob,fo):
     accuracy_score = true_num / ob_len
     return accuracy_score
 
-def hss(ob,fo):
-    accuracy_score = accuracy(ob,fo)
+def hss(ob,fo,threshold_list = None):
+
+    #参考accuracy 做扩展修改
+
+    accuracy_score = accuracy(ob,fo,threshold_list)
     ob_len = len(ob)
     ob1 = ob.reshape((-1))
     fo1 = fo.reshape((-1))
     ob2 = set(ob1)
     fo2 = set(fo1)
     results  = list(ob2|fo2)
+
+
     f_mult_o_sum = 0
     # 此处修改为不用循环
     for result in results:
@@ -34,8 +46,11 @@ def hss(ob,fo):
     HSS = (accuracy_score - (f_mult_o_sum/n_2))/(1-f_mult_o_sum/n_2)
     return  HSS
 
-def hk(ob,fo):
+def hk(ob,fo,threshold_list = None):
     # 多分类预报hss技巧评分
+
+    # 参考accuracy 做扩展修改
+
     accuracy_score = accuracy(ob, fo)
     ob_len = len(ob)
     ob1 = ob.reshape((-1))
