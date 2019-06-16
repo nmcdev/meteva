361e7a8e02cfc88ee4f4f3f581335d8580e72346
lianyipython <37836285+lianyipython@users.noreply.github.com>
2019-06-16 08:57:29 +0800

    Add files via upload

--- nmc_verification/nmc_vf_method/yes_or_no/__init__.py
+++ nmc_verification/nmc_vf_method/yes_or_no/__init__.py
@@ -1,6 +1,3 @@
 
-#import nmc_verification.nmc_vf_method.yes_or_no.threshold_one as threshold_one
-#import nmc_verification.nmc_vf_method.yes_or_no.threshold_list as threshold_list
-
 from . import threshold_list
 from . import threshold_one
