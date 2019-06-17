679a52a32d256b5febc1b164afd995baf84c5db5
liucouhua <liucouhua@163.com>
2019-06-17 22:40:00 +0800

    初步完成plot 透视模块

--- nmc_verification/nmc_vf_product/yes_or_no/score.py
+++ nmc_verification/nmc_vf_product/yes_or_no/score.py
@@ -27,3 +27,30 @@ def bias(sta,grade_list):
     ts_array = np.array(ts_list)
     ts_array = ts_array.reshape((fo_num,len(grade_list)))
     return ts_array,ts_list
+
+
+def mis_rate(sta,grade_list):
+    data_names = nmc_verification.nmc_vf_base.basicdata.get_data_names(sta)
+    ob = sta[data_names[0]].values
+    fo_num = len(data_names) - 1
+    ts_list = []
+    for i in range(fo_num):
+        fo = sta[data_names[i+1]].values
+        ts = nmc_verification.nmc_vf_method.yes_or_no.score.mis_rate(ob,fo,grade_list)
+        ts_list.append(ts.tolist())
+    ts_array = np.array(ts_list)
+    ts_array = ts_array.reshape((fo_num,len(grade_list)))
+    return ts_array,ts_list
+
+def fal_rate(sta,grade_list):
+    data_names = nmc_verification.nmc_vf_base.basicdata.get_data_names(sta)
+    ob = sta[data_names[0]].values
+    fo_num = len(data_names) - 1
+    ts_list = []
+    for i in range(fo_num):
+        fo = sta[data_names[i+1]].values
+        ts = nmc_verification.nmc_vf_method.yes_or_no.score.fal_rate(ob,fo,grade_list)
+        ts_list.append(ts.tolist())
+    ts_array = np.array(ts_list)
+    ts_array = ts_array.reshape((fo_num,len(grade_list)))
+    return ts_array,ts_list
