d8638d3aa99d93a9ad4f05c92b1ebad87324891e
lianyipython <37836285+lianyipython@users.noreply.github.com>
2019-07-22 17:12:18 +0800

    Add files via upload

--- nmc_verification/nmc_vf_base/basicdata/sta_data.py
+++ nmc_verification/nmc_vf_base/basicdata/sta_data.py
@@ -3,7 +3,12 @@ import copy
 import numpy as np
 
 def sta_data(df,columns = None):
-
+    '''
+    sta_data() 对数据进行格式化成为固定格式
+    :param df: dataframe的站点数据
+    :param columns: 文件内包含的数据的列名
+    :return: 包含level', 'time', 'dtime', 'id', 'lon', 'lat', 'alt 列的一个dataframe
+    '''
     #提取dframe0 列名称
     if columns is None:
         columns = df.columns
@@ -47,6 +52,11 @@ def sta_data(df,columns = None):
     return sta
 
 def get_undim_data_names(sta):
+    '''
+
+    :param sta:
+    :return:
+    '''
     coor_columns = ['level', 'time', 'dtime', 'id', 'lon', 'lat', 'alt']
     columns = sta.columns
     data_columns = []
@@ -57,6 +67,11 @@ def get_undim_data_names(sta):
     return data_columns
 
 def get_data_names(sta):
+    '''
+    get_data_names() 获取站点数据的要素名
+    :param sta: 站点数据
+    :return: 要素名列表
+    '''
     coor_columns = ['level', 'time', 'dtime', 'id', 'lon', 'lat', 'alt']
     columns = sta.columns
     data_columns = []
@@ -66,14 +81,50 @@ def get_data_names(sta):
     return data_columns
 
 def get_coord_names():
+    '''
+    :return: 站点数据基本信息列名['level', 'time', 'dtime', 'id', 'lon', 'lat', 'alt']列表
+    '''
     return ['level', 'time', 'dtime', 'id', 'lon', 'lat', 'alt']
 
 def set_data_name(sta,data_name):
+    '''
+    更改 要素名，和添加缺省列
+    :param sta: 站点数据
+    :param data_name: 站点数据 要素名
+    :return: 更改要素名名后的站点数据
+    '''
     coor_columns = ['level', 'time', 'dtime', 'id', 'lon', 'lat', 'alt',data_name]
     sta.columns = coor_columns
     return
 
+def reset_id(sta):
+    '''
+    将带有a-z,A-Z格式的按照，通过ASCII码转换为数字，拼接完之后，再返回
+    例如：sta = 'abdf' 返回：9798100102
+    :param sta:站号
+    :return:ASCII转码之后的int型站号。
+    '''
+    a_list = []
+    b_list = []
+    for item in sta:
+        a_list.append(item)
+    num = len(a_list)
+    for i in range(0, num):
+        num = ord(a_list[i])
+        b_list.append(str(num))
+    item2 = "".join(b_list)
+    return int(item2)
+
 def set_time_dtime_level_name(sta,time = None,dtime = None,level = None,data_name = None):
+    '''
+    set_time_dtime_level_name 设置time_dtime_level 的值  并且设置要素名
+    :param sta: 站点数据
+    :param time: 起报时
+    :param dtime: 时效
+    :param level: 层次
+    :param data_name: 要素名
+    :return:  站点数据
+    '''
     if time is not None:
         sta['time'] = time
     if dtime is not None:
@@ -82,19 +133,4 @@ def set_time_dtime_level_name(sta,time = None,dtime = None,level = None,data_nam
         sta['level'] = level
     if data_name is not None:
         set_data_name(sta,data_name)
-def reset_id(sta):
-    values = sta['id'].values
-    if type(values[0]) == str:
-        int_id = np.zeros(len(values))
-        for i in range(len(values)):
-            strs = values[i]
-            strs_int = ""
-            for s in strs:
-                if s.isdigit():
-                    strs_int += s
-                else:
-                    strs_int += str(ord(s))
-            int_id[i] = int(strs_int)
-        int_id = int_id.astype(np.int32)
-        sta['id'] = int_id
-    return
+
