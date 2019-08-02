06143e8e2edb023e9c99e21e2fcca70e2bbcf9fd
1002803832 <36762364+1002803832@users.noreply.github.com>
2019-08-02 10:48:54 +0800

    Add files via upload

--- nmc_verification/nmc_vf_method/multi_category/score.py
+++ nmc_verification/nmc_vf_method/multi_category/score.py
@@ -1,92 +1,70 @@
-import numpy as np
-
-def accuracy(ob,fo,grade_list = None):
-    '''
-    accuracy 求多分类预报准确率
-    :param ob: 实况数据 一维numpy
-    :param fo:  预测数据 一维numpy
-    :param grade_list:等级
-    :return:
-    '''
-    #多分类预报准确率
-    if grade_list is None:
-        #ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-
-        ob1 = ob.reshape((-1))
-        fo1 = fo.reshape((-1))
-    else:
-        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
-        ob1 = None #此处需修改
-        fo1 = None #此处需修改
-
-    ob_len = len(ob1)
-    true_num = 0
-
-    #为了提高计算效率，此处需修改为不用循环的方式实现计算，后续其他开发也需要如此
-    for i in range(ob_len):
-        if ob1[i] == fo1[i]:
-            true_num += 1
-    accuracy_score = true_num / ob_len
-    return accuracy_score
-
-def hss(ob,fo,grade_list = None):
-    '''
-    hss heidke技能得分
-    :param ob: 实况数据 一维numpy
-    :param fo: 预测数据 一维numpy
-    :param grade_list: 等级
-    :return:
-    '''
-    #参考accuracy 做扩展修改
-
-    accuracy_score = accuracy(ob,fo,grade_list)
-    ob_len = len(ob)
-    ob1 = ob.reshape((-1))
-    fo1 = fo.reshape((-1))
-    ob2 = set(ob1)
-    fo2 = set(fo1)
-    results  = list(ob2|fo2)
+import nmc_verification
+import datetime
 
 
-    f_mult_o_sum = 0
+def accuracy_muti_model(ob_sta, fo_sta_list, grade_list):
+    intersection_of_data = fo_sta_list[0]
+    for i in range(1, len(fo_sta_list) - 1):
+        intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+            intersection_of_data, fo_sta_list[i])
+    intersection_of_data['dtime'] = intersection_of_data['dtime'].map(lambda x: datetime.timedelta(hours=x))
+    intersection_of_data['time'] = intersection_of_data['time'] + intersection_of_data['dtime']
+    intersection_of_data['dtime'] = 0
+    intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+        intersection_of_data, ob_sta)
+    intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+        intersection_of_data, ob_sta)
+    ob_data = intersection_of_data.iloc[:, -1]
+    ob_data = ob_data.values
+    accuracy_list = []
+    for fo_of_data in intersection_of_data.iloc[:, 7:-1]:
+        fo_of_data = fo_of_data.values
+        accuracy_score = nmc_verification.nmc_vf_method.multi_category.score.accuracy(ob_data, fo_of_data, grade_list)
+        accuracy_list.append(accuracy_score)
 
-    for result in results:
-        NF = np.sum(ob1 == result)
-        NO = np.sum(fo1 == result )
-        f_mult_o = NF*NO
-        f_mult_o_sum += f_mult_o
-    n_2 = ob_len**2
-    HSS = (accuracy_score - (f_mult_o_sum/n_2))/(1-f_mult_o_sum/n_2)
-    return  HSS
+    return accuracy_list
 
-def hk(ob,fo,grade_list = None):
-    '''
-    hk Hanssen和Kuipers判别
-    :param ob: 实况数据 一维numpy
-    :param fo: 预测数据 一维numpy
-    :param grade_list: 等级
-    :return:
-    '''
-    # 多分类预报hss技巧评分
 
-    # 参考accuracy 做扩展修改
+def hss_muti_model(ob_sta, fo_sta_list, grade_list):
+    intersection_of_data = fo_sta_list[0]
+    for i in range(1, len(fo_sta_list) - 1):
+        intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+            intersection_of_data, fo_sta_list[i])
+    intersection_of_data['dtime'] = intersection_of_data['dtime'].map(lambda x: datetime.timedelta(hours=x))
+    intersection_of_data['time'] = intersection_of_data['time'] + intersection_of_data['dtime']
+    intersection_of_data['dtime'] = 0
+    intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+        intersection_of_data, ob_sta)
+    intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+        intersection_of_data, ob_sta)
+    ob_data = intersection_of_data.iloc[:, -1]
+    ob_data = ob_data.values
+    hss_list = []
+    for fo_of_data in intersection_of_data.iloc[:, 7:-1]:
+        fo_of_data = fo_of_data.values
+        hss_score = nmc_verification.nmc_vf_method.multi_category.score.hss(ob_data, fo_of_data, grade_list)
+        hss_list.append(hss_score)
+    return hss_list
 
-    accuracy_score = accuracy(ob, fo)
-    ob_len = len(ob)
-    ob1 = ob.reshape((-1))
-    fo1 = fo.reshape((-1))
-    ob2 = set(ob1)
-    fo2 = set(fo1)
-    results = list(ob2 | fo2)
-    f_mult_o_sum = 0
-    No_2_sum = 0
 
-    for result in results:
-        NO = np.sum(ob1 == result)
-        NF = np.sum(fo1 == result)
-        f_mult_o = NF * NO
-        f_mult_o_sum += f_mult_o
-        No_2_sum +=  NO**2
-    N_2 = ob_len ** 2
-    HK_score = (accuracy_score- (f_mult_o_sum/N_2))/(1-No_2_sum/N_2)
-    return  HK_score
+def hk_muti_model(ob_sta, fo_sta_list, grade_list):
+    intersection_of_data = fo_sta_list[0]
+    for i in range(1, len(fo_sta_list) - 1):
+        intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+            intersection_of_data, fo_sta_list[i])
+    intersection_of_data['dtime'] = intersection_of_data['dtime'].map(lambda x: datetime.timedelta(hours=x))
+    intersection_of_data['time'] = intersection_of_data['time'] + intersection_of_data['dtime']
+    intersection_of_data['dtime'] = 0
+    intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+        intersection_of_data, ob_sta)
+    intersection_of_data = nmc_verification.nmc_vf_base.function.put_into_sta_data.merge_on_all_dim(
+        intersection_of_data, ob_sta)
+    ob_data = intersection_of_data.iloc[:, -1]
+    ob_data = ob_data.values
+    hk_list = []
+    for fo_of_data in intersection_of_data.iloc[:, 7:-1]:
+        fo_of_data = fo_of_data.values
+        hk_score = nmc_verification.nmc_vf_method.multi_category.score.hk(ob_data, fo_of_data, grade_list)
+        hk_list.append(hk_score)
+
+    return hk_list
