79130bdf753b5b088165be614812cfab81653a0a
1002803832 <36762364+1002803832@users.noreply.github.com>
2019-08-09 09:54:00 +0800

    Add files via upload

--- nmc_verification/nmc_vf_method/continuous/score.py
+++ nmc_verification/nmc_vf_method/continuous/score.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def me(Ob,Fo):
+
+def me(Ob, Fo):
     '''
     me 求两组数据的误差平均值
     -----------------------------
@@ -11,7 +12,8 @@ def me(Ob,Fo):
     mean_error = np.mean(Fo - Ob)
     return mean_error
 
-def mae(Ob,Fo):
+
+def mae(Ob, Fo):
     '''
     mae对两组数据求平均绝对值误差
     -----------------------
@@ -19,10 +21,11 @@ def mae(Ob,Fo):
     :param Fo:预测数据 一维numpy
     :return: mean_abs_error
     '''
-    mean_abs_error = np.mean(np.abs(Ob-Fo))
+    mean_abs_error = np.mean(np.abs(Ob - Fo))
     return mean_abs_error
 
-def mse(Ob,Fo):
+
+def mse(Ob, Fo):
     '''
     mse  求两组数据的均方误差
     ----------------------------------
@@ -31,9 +34,10 @@ def mse(Ob,Fo):
     :return: mean_sqrt_error
     '''
     mean_sqrt_error = np.mean(np.square(Ob - Fo))
-    return  mean_sqrt_error
+    return mean_sqrt_error
 
-def rmse(Ob,Fo):
+
+def rmse(Ob, Fo):
     '''
     rmse 求两组数据的均方根误差
     ------------------------------
@@ -44,7 +48,8 @@ def rmse(Ob,Fo):
     mean_sqrt_error = np.sqrt(np.mean(np.square(Ob - Fo)))
     return mean_sqrt_error
 
-def bias(Ob,Fo):
+
+def bias(Ob, Fo):
     '''
     bias 求预测数据和实况数据的平均值的比
     ------------------------------
@@ -55,7 +60,8 @@ def bias(Ob,Fo):
     bias0 = np.mean(Fo) / (np.mean(Ob) + 1e-6)
     return bias0
 
-def corr(Ob,Fo):
+
+def corr(Ob, Fo):
     '''
     corr 求实况数据还和预测数据之间的相关系数
     -----------------------------
@@ -65,45 +71,57 @@ def corr(Ob,Fo):
     '''
     ob_f = Ob.flatten()
     fo_f = Fo.flatten()
-    corr0 = np.corrcoef(ob_f,fo_f)[0,1]
+    corr0 = np.corrcoef(ob_f, fo_f)[0, 1]
     return corr0
 
-#平均相对误差
-def are(Ob,Fo):
+
+# 平均相对误差
+def are(Ob, Fo):
+    '''
+    are  平均绝对值误差
+    :param Ob: 实况数据 不定长维度的numpy
+    :param Fo: 测试数据 不定长维度的numpy
+    :return: are
+    '''
     s = Ob + Fo
-    if np.sum(s) ==0:
+    if np.sum(s) == 0:
         return 0
     else:
         d = Ob - Fo
-        s1 = s[s>0]
-        d1 = d[s>0]
+        s1 = s[s > 0]
+        d1 = d[s > 0]
 
-        are0 = np.mean(np.abs(d1/s1))
+        are0 = np.mean(np.abs(d1 / s1))
         return are0
 
-#FSS
-def FSS(Ob,Fo,window_sizes_list = [3],threshold_list = [50],Masker = None):
+
+# FSS
+def FSS(Ob, Fo, window_sizes_list=[3], threshold_list=[50], Masker=None):
     shape = Ob.shape
     nw = len(window_sizes_list)
+    # print(nw)
     nt = len(threshold_list)
-    fss = np.zeros((nw,nt))
+    fss = np.zeros((nw, nt))
     for i in range(nw):
-        kernel = np.ones((nw,nw))
+        kernel = np.ones((nw, nw))
+        # print(kernel)
         ws = np.sum(kernel)
         if Masker is not None:
-            masker_sum = np.convolve(Masker, kernel,mode= "same") + 1e-10
+            masker_sum = np.convolve(Masker, kernel, mode="same") + 1e-10
         else:
             masker_sum = np.ones(shape) * ws + 1e-10
         for j in range(nt):
             ob_hap = np.zeros(shape)
-            ob_hap[Ob>threshold_list[j]] = 1
+            ob_hap[Ob > threshold_list[j]] = 1
             fo_hap = np.zeros(shape)
-            ob_hap[Ob>threshold_list[j]] = 1
-            ob_hap_sum = np.convolve(ob_hap,kernel,mode = "same")
-            fo_hap_sum = np.convolve(fo_hap,kernel,mode = "same")
-            ob_hap_p = ob_hap_sum/masker_sum
-            fo_hap_p = fo_hap_sum/masker_sum
-            a1 = np.sum(np.power(ob_hap_p - fo_hap_p,2))
-            a2 = np.sum(np.power(ob_hap_p,2)) + np.sum(np.power(fo_hap_p,2))
-            fss[i,j] = 1 - a1 / (a2 + 1e-10)
+            fo_hap[Fo > threshold_list[j]] = 1
+            ob_hap_sum = np.convolve(ob_hap, kernel, mode="same")
+            fo_hap_sum = np.convolve(fo_hap, kernel, mode="same")
+            ob_hap_p = ob_hap_sum / masker_sum
+            fo_hap_p = fo_hap_sum / masker_sum
+            a1 = np.sum(np.power(ob_hap_p - fo_hap_p, 2))
+            a2 = np.sum(np.power(ob_hap_p, 2)) + np.sum(np.power(fo_hap_p, 2))
+            fss[i, j] = 1 - a1 / (a2 + 1e-10)
     return fss
+
+
