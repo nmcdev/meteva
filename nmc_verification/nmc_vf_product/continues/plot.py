25f69faf31539a2484967024ff46d2654c04f7e0
1002803832 <36762364+1002803832@users.noreply.github.com>
2019-09-19 10:23:03 +0800

    Add files via upload

--- nmc_verification/nmc_vf_product/continues/plot.py
+++ nmc_verification/nmc_vf_product/continues/plot.py
@@ -8,7 +8,7 @@ import nmc_verification.nmc_vf_base.function.put_into_sta_data as pisd
 
 
 def scatter_regress_muti_model(ob, fo_df_list, save_path=None, scattercolor='r', scattersize=5,
-                               x_label='fo', y_label='bo', fontsize=10, line_color='r'):
+                               x_label='bo', y_label='fo', fontsize=10, line_color='r'):
     '''
     scatter_regress_muti_model  多模式下画一张带有回归线的实况和预报数据的散点图
     :param ob:一个实况数据  类型  dataframe
@@ -24,7 +24,6 @@ def scatter_regress_muti_model(ob, fo_df_list, save_path=None, scattercolor='r',
     '''
     fo_df_list.append(ob)
     meger_df_data = pisd.merge_on_id_and_obTime(fo_df_list)
-
     ob = meger_df_data.iloc[:, -1].values
     data_len = len(fo_df_list)
     plt.figure(figsize=[6.4 * data_len, 4.8])
@@ -37,7 +36,6 @@ def scatter_regress_muti_model(ob, fo_df_list, save_path=None, scattercolor='r',
             title = title + str(the_duplicate_values[0])
     plt.suptitle(title)
     for index, fo_of_colnum in enumerate(meger_df_data.iloc[:, 7:-1]):
-
         fo = meger_df_data[fo_of_colnum].values
         plt.subplot(1, data_len, index + 1)
         plt.plot(ob, fo, 'o', markerfacecolor=scattercolor, markersize=scattersize)
@@ -66,7 +64,9 @@ def scatter_regress_muti_model(ob, fo_df_list, save_path=None, scattercolor='r',
     else:
         plt.savefig(save_path)
 
-def box_plot_muti_model(ob, fo_df_list, save_path=None, x_lable='observation', y_lable='forecast', title='box-plot'):
+
+# 需要更改
+def box_plot_muti_model(ob, fo_df_list, save_path=None, lables=None):
     '''
     box_plot 画一多模式数据的箱型图
     ---------------
@@ -80,22 +80,21 @@ def box_plot_muti_model(ob, fo_df_list, save_path=None, x_lable='observation', y
     '''
     fo_df_list.append(ob)
     meger_df_data = pisd.merge_on_id_and_obTime(fo_df_list)
-
-    colnums = ['level', 'id', 'time']
     title = ''
+    colnums = ['level', 'id', 'time']
     for colnum in colnums:
         the_duplicate_values = meger_df_data[colnum].unique()
         if len(the_duplicate_values) == 1:
             title = title + str(the_duplicate_values[0])
-
     ob = meger_df_data.iloc[:, -1]
     meger_df_data.drop(meger_df_data.columns[-1], axis=1, inplace=True)
     meger_df_data.insert(7, 'ob', ob)
-    labels = meger_df_data.columns[7:]
+    if lables is None:
+        labels = meger_df_data.columns[7:]
     ob_and_fo_data = meger_df_data.iloc[:, 7:]
     ob_and_fo_ndarray_T = ob_and_fo_data.values.T
     ob_and_fo_tuple_T = tuple(ob_and_fo_ndarray_T)
-    plt.boxplot(ob_and_fo_tuple_T, labels=labels)
+    plt.boxplot(ob_and_fo_tuple_T, labels=labels, whis=(0, 100))
     plt.title(title)
     if save_path is None:
         plt.show()
@@ -103,13 +102,18 @@ def box_plot_muti_model(ob, fo_df_list, save_path=None, x_lable='observation', y
         plt.savefig(save_path)
 
 
-def sorted_ob_fo_muti_model(ob, fo_list, save_path=None):
+# 需要更改
+def sorted_ob_fo_muti_model(ob, fo_list, save_path=None, x_label='ob', y_label='fo', fontsize=10, line_color='b'):
     '''
     sorted_ob_fo_muti_model 多模式下先对数据排序，然后在画多个折线图子图
     :param ob:一个实况数据  类型  dataframe
     :param fo_list:多模式预测数据 列表  类型list  list中的类型是dataframe
     每个dataframe 中的最后一列列名不能相同，表示时空数据的列为在前，列名相同
     :param save_path:保存地址
+    :param x_label  x轴标签
+    :param y_label  y轴标签
+    :param fontsize  字体大小
+    :param line_color 线的颜色
     :return:
     '''
     fo_list.append(ob)
@@ -118,9 +122,8 @@ def sorted_ob_fo_muti_model(ob, fo_list, save_path=None):
 
     data_len = len(fo_list)
     plt.figure(figsize=[10 * data_len, 4.8])
-
-    colnums = ['level', 'id', 'time']
     title = ''
+    colnums = ['level', 'id', 'time']
     for colnum in colnums:
         the_duplicate_values = meger_df_data[colnum].unique()
 
@@ -132,7 +135,10 @@ def sorted_ob_fo_muti_model(ob, fo_list, save_path=None):
         plt.subplot(1, data_len, index + 1)
         ob_sorted = np.sort(ob)
         fo_sorted = np.sort(fo)
-        plt.plot(fo_sorted, ob_sorted)
+        plt.plot(fo_sorted, ob_sorted, line_color)
+        plt.xlabel(x_label, size=fontsize)
+
+        plt.ylabel(y_label, size=fontsize)
 
     if save_path is None:
         plt.show()
