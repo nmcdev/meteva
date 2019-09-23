414f716ad5053738a1266ddf5800d828ceda0a0e
crafty <1002803832@qq.com>
2019-09-23 16:36:30 +0800

    Update skill.py

--- nmc_verification/nmc_vf_method/yes_or_no/skill.py
+++ nmc_verification/nmc_vf_method/yes_or_no/skill.py
@@ -1,8 +1,7 @@
-
-#计算晴雨预报相对技巧
-def spc(pc,pc_base):
+# 计算晴雨预报相对技巧
+def spc(pc, pc_base):
     if pc_base == 1:
-        if pc <1:
+        if pc < 1:
             return -999
         else:
             return 0
@@ -10,16 +9,18 @@ def spc(pc,pc_base):
         spc1 = (pc - pc_base) / (1 - pc_base)
         return spc1
 
-#计算Ts评分相对技巧
-def sts(ts,ts_base):
+
+# 计算Ts评分相对技巧
+def sts(ts, ts_base):
     if ts_base == 1:
         return 9999
     else:
         sts1 = (ts - ts_base) / (1 - ts_base)
         return sts1
 
-def sbi(be,be_base):
-    if be_base ==0:
+
+def sbi(be, be_base):
+    if be_base == 0:
         return 9999
     else:
         sbi1 = (be_base - be) / be_base
