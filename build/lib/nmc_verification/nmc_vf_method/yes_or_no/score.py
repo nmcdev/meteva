ed3b960a5249be9eccaa981702f6ecff74999abf
liucouhua <liucouhua@163.com>
2019-12-01 22:02:02 +0800

    改进了评分方法
    改进了每个函数的设计，使得其能够兼容任意维的数组作为输入，同时改进后的算法使得在需要循环获取每个部分的hmfn时代码会更加简洁。

--- build/lib/nmc_verification/nmc_vf_method/yes_or_no/score.py
+++ build/lib/nmc_verification/nmc_vf_method/yes_or_no/score.py
@@ -1,6 +1,24 @@
-
 import numpy as np
 import copy
+
+def pc_hmfn(hmfn_list):
+    '''
+    晴雨准确率
+    -------------------------
+    :param hit: 命中数
+    :param mis: 空报数
+    :param fal: 漏报数
+    -------------------------
+    :return:
+    '''
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    hit = hmfn_array_sum[0]
+    mis = hmfn_array_sum[1]
+    fal = hmfn_array_sum[2]
+    cn = hmfn_array_sum[3]
+    cr = (hit + cn) / (hit + mis + fal + cn)
+    return cr
 
 def pc_of_sunny_rainy(Ob, Fo):
     '''
@@ -10,10 +28,9 @@ def pc_of_sunny_rainy(Ob, Fo):
     :return:
     '''
     # 晴雨准确率
-    hit, mis, fal, cn = hmfn_of_sunny_rainy(Ob, Fo)
-    cr = (hit + cn) / (hit + mis + fal + cn)
+    hmfn_list = hmfn_of_sunny_rainy(Ob, Fo)
+    cr = pc_hmfn(hmfn_list)
     return cr
-
 
 def hmfn_of_sunny_rainy(Ob, Fo):
     '''
@@ -25,12 +42,12 @@ def hmfn_of_sunny_rainy(Ob, Fo):
     # 晴雨准确率
     fo1 = copy.deepcopy(Fo)
     fo1[fo1 < 0.099] = 0
-    num = np.size(Ob)
-    obhap = np.zeros(num)
+    shape = Ob.shape
+    obhap = np.zeros(shape)
     obhap[Ob > 0] = 1
-    fohap = np.zeros(num)
+    fohap = np.zeros(shape)
     fohap[fo1 > 0] = 1
-    obhap01 = np.zeros(num)
+    obhap01 = np.zeros(shape)
     obhap01[Ob >= 0.1] = 1
 
     hit_threshold = (obhap * fohap)
@@ -42,27 +59,20 @@ def hmfn_of_sunny_rainy(Ob, Fo):
     mis = mis_threshold.sum()
     fal = fal_threshold.sum()
     cn = cn_threshold.sum()
-    return hit, mis, fal, cn
+    return hit.tolist(), mis.tolist(), fal.tolist(), cn.tolist()
 
-
-def hit_rate(Ob, Fo, grade_list=[1e-300]):
-    '''
-    hit_rate 求出命中率
-    ----------------------
-    :param Ob: 实况数据 一维numpy
-    :param Fo: 预测数据 一维numpy
-    :param grade_list: 等级
-    :return:
-    '''
-    # 输入观测Ob和预报Fo的预报数据（1维的numpy数组），以及判断事件是否发生的阈值threshold，
-    # 返回一维命中率评分值数组，数组中的每个值对应一个等级
-    # 如果threshold_list ==None，则说明Ob,Fo是0或1组成的数组
-
+def hit_rate(Ob, Fo, grade_list=[1e-30]):
     hit, mis, fal, _ = hmfn(Ob, Fo, grade_list)
     return hit / (hit + mis + 0.0000001)
 
+def hit_rate_hmfn(hmfn_list):
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    hit = hmfn_array_sum[0]
+    mis = hmfn_array_sum[1]
+    return hit / (hit + mis + 0.0000001)
 
-def fal_rate(Ob, Fo, grade_list=[1e-300]):
+def fal_rate(Ob, Fo, grade_list=[1e-30]):
     '''
     fal-rate  求出误报率
     ------------------
@@ -78,8 +88,14 @@ def fal_rate(Ob, Fo, grade_list=[1e-300]):
     hit, mis, fal, _ = hmfn(Ob, Fo, grade_list)
     return fal / (hit + fal + 0.0000001)
 
+def fal_rate_hmfn(hmfn_list):
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    hit = hmfn_array_sum[0]
+    fal = hmfn_array_sum[2]
+    return fal / (hit + fal + 0.0000001)
 
-def mis_rate(Ob, Fo, grade_list=[1e-300]):
+def mis_rate(Ob, Fo, grade_list=[1e-30]):
 
     '''
     mis_rate 漏报率评分
@@ -96,8 +112,14 @@ def mis_rate(Ob, Fo, grade_list=[1e-300]):
     hit, mis, fal, _ = hmfn(Ob, Fo, grade_list)
     return mis / (hit + mis + 0.0000001)
 
+def mis_rate_hmfn(hmfn_list):
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    mis = hmfn_array_sum[1]
+    hit = hmfn_array_sum[0]
+    return mis / (hit + mis + 0.0000001)
 
-def bias(Ob, Fo, grade_list=[1e-300]):
+def bias(Ob, Fo, grade_list=[1e-30]):
     '''
     bias   bias评分
     ----------------
@@ -117,16 +139,30 @@ def bias(Ob, Fo, grade_list=[1e-300]):
     bias0[delta ==0] = 1
     return bias0
 
+def bias_hmfn(hmfn_list):
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    mis = hmfn_array_sum[1]
+    hit = hmfn_array_sum[0]
+    fal = hmfn_array_sum[2]
+    bias0 = (hit + fal) / (hit + mis + 0.0000001)
+    sum = hit +mis +fal
+    bias0[sum == 0] = 1
+    delta = fal - mis
+    bias0[delta ==0] = 1
+    return bias0
 
-
-def bias_extend(Ob, Fo, grade_list=[1e-300]):
+def bias_extend(Ob, Fo, grade_list=[1e-30]):
     bias0 = bias(Ob, Fo,grade_list)
-
     bias_extend0 = np.abs(bias0 - 1)
     return bias_extend0
 
+def bias_extend_hmfn(hmfn_list):
+    bias0 = bias_hmfn(hmfn_list)
+    bias_extend0 = np.abs(bias0 - 1)
+    return bias_extend0
 
-def ts(Ob, Fo, grade_list=[1e-300]):
+def ts(Ob, Fo, grade_list=[1e-30]):
     '''
     ts ts评分
     -----------------------------
@@ -139,12 +175,10 @@ def ts(Ob, Fo, grade_list=[1e-300]):
     # 输入观测Ob和预报Fo的预报数据（1维的numpy数组），以及判断事件是否发生的阈值value，
     # 返回一维ts评分值数组，数组中的每个值对应一个等级
     # 如果threshold_list ==None，则说明Ob,Fo是0或1组成的数组
+    hmfn_list = [hmfn(Ob, Fo, grade_list)]
+    return ts_hmfn(hmfn_list)
 
-    hit, mis, fal, _ = hmfn(Ob, Fo, grade_list)
-    return ts_hmfn(hit, mis, fal)
-
-
-def ts_hmfn(hit, mis, fal):
+def ts_hmfn(hmfn_list):
     '''
     ts  hmfn评分
     -------------------------
@@ -156,10 +190,15 @@ def ts_hmfn(hit, mis, fal):
     '''
     # 输入命中、空报、漏报数
     # 返回一维ts评分值数组，数组中的每个值对应一个等级
-    return hit / (hit + mis + fal + 0.000001)
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    hit = hmfn_array_sum[0]
+    mis = hmfn_array_sum[1]
+    fal = hmfn_array_sum[2]
+    ts_array =hit / (hit + mis + fal + 1e-30)
+    return ts_array
 
-
-def ets(Ob, Fo, grade_list=[1e-300]):
+def ets(Ob, Fo, grade_list=[1e-30]):
     '''
     ets ets评分
     ----------------------------
@@ -172,12 +211,10 @@ def ets(Ob, Fo, grade_list=[1e-300]):
     # 输入观测Ob和预报Fo的预报数据（1维的numpy数组），以及判断事件是否发生的阈值value，
     # 返回一维ets评分值数组，数组中的每个值对应一个等级
     # 如果threshold_list ==None，则说明Ob,Fo是0或1组成的数组
+    hmfn_list = [hmfn(Ob, Fo, grade_list)]
+    return ets_hmfn(hmfn_list)
 
-    hit, mis, fal, cn = hmfn(Ob, Fo, grade_list)
-    return ets_hmfn(hit, mis, fal, cn)
-
-
-def ets_hmfn(hit, mis, fal, cn):
+def ets_hmfn(hmfn_list):
     '''
     ets_hmfn ets_hmfn评分# 输入命中数、空报数、漏报数、正确否定数
     # 返回一维ets评分值数组，数组中的每个值对应一个等级
@@ -189,11 +226,16 @@ def ets_hmfn(hit, mis, fal, cn):
     -------------------------
     :return:
     '''
+    hmfn_array = np.array(hmfn_list)
+    hmfn_array_sum = np.sum(hmfn_array,axis=0)
+    hit = hmfn_array_sum[0]
+    mis = hmfn_array_sum[1]
+    fal = hmfn_array_sum[2]
+    cn = hmfn_array_sum[3]
 
     total = hit + mis + fal + cn + 0.000001  # 加0.0000001 为防止出现除0情况
     hit_random = (hit + mis) * (hit + fal) / total
     return (hit - hit_random) / (hit + mis + fal - hit_random + 0.000001)
-
 
 def hmfn(Ob, Fo, grade_list=[1e-300]):
     '''
@@ -216,9 +258,9 @@ def hmfn(Ob, Fo, grade_list=[1e-300]):
     for i in range(len(grade_list)):
         threshold = grade_list[i]
         num = np.size(Ob)
-        obhap = np.zeros(num)
+        obhap = np.zeros_like(Ob)
         obhap[Ob >= threshold] = 1
-        fohap = np.zeros(num)
+        fohap = np.zeros_like(Fo)
         fohap[Fo >= threshold] = 1
 
         hit_threshold = (obhap * fohap)
@@ -230,3 +272,5 @@ def hmfn(Ob, Fo, grade_list=[1e-300]):
         fal[i] = fal_threshold.sum()
         cn[i] = cn_threshold.sum()
     return hit, mis, fal, cn
+
+
