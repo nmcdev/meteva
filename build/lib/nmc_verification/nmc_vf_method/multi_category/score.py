0ce5385225a040e01328692c97174b8dfa0b72dd
liucouhua <liucouhua@163.com>
2019-12-02 22:31:31 +0800

    改进了多分类评分程序
    使得可以计算中间结果，并根据中间结果进行计算

--- build/lib/nmc_verification/nmc_vf_method/multi_category/score.py
+++ build/lib/nmc_verification/nmc_vf_method/multi_category/score.py
@@ -1,93 +1,53 @@
-'''
-
-改过     未提交
-
-'''
+import nmc_verification
 import numpy as np
-
 
 def accuracy(ob, fo, grade_list=None):
     '''
     accuracy 求多分类预报准确率
-    :param ob: 实况数据 一维numpy
-    :param fo:  预测数据 一维numpy
-    :param grade_list:等级
+    :param ob: 实况数据 不确定维numpy
+    :param fo:  预测数据 不确定维numpy
+    :param grade_list:等级，如果grade_list= None则ob和fo里的值代表类别，否则，根据grade_list来进行分类
     :return:
     '''
     # 多分类预报准确率
     if grade_list is None:
-        # ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-
+        # ob 和fo是不确定维的numpy数组，其中每个值是代表分类的类别
         ob1 = ob.reshape((-1))
         fo1 = fo.reshape((-1))
     else:
+        ob1 = np.zeros_like(ob)
+        fo1 = np.zeros_like(fo)
         # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
         for index in range(len(grade_list) - 1):
             ob_index_list = np.where((grade_list[index] <= ob) & (ob < grade_list[index + 1]))
-            ob[ob_index_list] = grade_list[index]
+            ob1[ob_index_list] = index+1
             fo_index_list = np.where((grade_list[index] <= fo) & (fo < grade_list[index + 1]))
-            fo[fo_index_list] = grade_list[index]
+            fo1[fo_index_list] = index+1
         ob_index_list = np.where(grade_list[-1] <= ob)
-        ob[ob_index_list] = grade_list[-1]
+        ob1[ob_index_list] = len(grade_list)
         fo_index_list = np.where(grade_list[-1] <= fo)
-        fo[fo_index_list] = grade_list[-1]
-
-        ob1 = ob  # 此处需修改
-        fo1 = fo  # 此处需修改
-
-    ob_len = len(ob1)
-
-    true_num = np.sum(fo1 == ob1)
-
-    accuracy_score = true_num / ob_len
+        fo1[fo_index_list] = len(grade_list)
+    correct_num = np.sum(fo1 == ob1)
+    accuracy_score = correct_num / ob.size
     return accuracy_score
 
-
-def  hss(ob, fo, grade_list=None):
+def hss(ob,fo,grade_list = None):
     '''
-    hss heidke技能得分
+    hss heidke技能得分,它表现实际的预报的分类准确性相对于随机分类达到的准确性的技巧
     :param ob: 实况数据 一维numpy
     :param fo: 预测数据 一维numpy
     :param grade_list: 等级
     :return:
     '''
-    # 参考accuracy 做扩展修改
-
+    conf_mx = nmc_verification.nmc_vf_method.multi_category.table.contingency_table(ob,fo,grade_list)
     accuracy_score = accuracy(ob, fo, grade_list)
-    if grade_list is None:
-        # ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-
-        ob1 = ob.reshape((-1))
-        fo1 = fo.reshape((-1))
-    else:
-        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
-        for index in range(len(grade_list) - 1):
-
-            ob_index_list = np.where((grade_list[index] <= ob) & (ob < grade_list[index + 1]))
-            ob[ob_index_list] = grade_list[index]
-            fo_index_list = np.where((grade_list[index] <= fo) & (fo < grade_list[index + 1]))
-            fo[fo_index_list] = grade_list[index]
-        ob_index_list = np.where(grade_list[-1] <= ob)
-        ob[ob_index_list] = grade_list[-1]
-        fo_index_list = np.where(grade_list[-1] <= fo)
-        fo[fo_index_list] = grade_list[-1]
-        ob1 = ob  # 此处需修改
-        fo1 = fo  # 此处需修改
-
-    ob2 = set(ob1)
-    fo2 = set(fo1)
-    ob_len = len(ob2)
-    results = list(ob2 | fo2)
-
-    f_mult_o_sum = 0
-    for result in results:
-        NF = np.sum(ob1 == result)
-        NO = np.sum(fo1 == result)
-        f_mult_o = NF * NO
-        f_mult_o_sum += f_mult_o
-    n_2 = ob_len ** 2
-    HSS = (accuracy_score - (f_mult_o_sum / n_2)) / (1 - f_mult_o_sum / n_2)
+    total_num = ob.size
+    NF_array = conf_mx[0:-1,-1]
+    NO_array = conf_mx[-1,0:-1]
+    random_score = np.dot(NF_array,NO_array) /(total_num * total_num)
+    HSS = (accuracy_score - random_score)/(1 - random_score)
     return HSS
+
 
 
 def hk(ob, fo, grade_list=None):
@@ -98,45 +58,17 @@ def hk(ob, fo, grade_list=None):
     :param grade_list: 等级
     :return:
     '''
-    # 多分类预报hss技巧评分
+    # 多分类预报hk技巧评分
 
-    # 参考accuracy 做扩展修改
-
+    conf_mx = nmc_verification.nmc_vf_method.multi_category.table.contingency_table(ob,fo,grade_list)
     accuracy_score = accuracy(ob, fo, grade_list)
-    ob_len = len(ob)
-    if grade_list is None:
-        # ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
+    total_num = ob.size
+    NF_array = conf_mx[0:-1,-1]
+    NO_array = conf_mx[-1,0:-1]
+    random_score = np.dot(NF_array,NO_array) /(total_num * total_num)
+    ob_rate = np.dot(NO_array,NO_array)/(total_num * total_num)
+    HK = (accuracy_score - random_score)/(1 - ob_rate)
 
-        ob1 = ob.reshape((-1))
-        fo1 = fo.reshape((-1))
-    else:
-        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
-        for index in range(len(grade_list) - 1):
-
-            ob_index_list = np.where((grade_list[index] <= ob) & (ob < grade_list[index + 1]))
-            ob[ob_index_list] = grade_list[index]
-            fo_index_list = np.where((grade_list[index] <= fo) & (fo < grade_list[index + 1]))
-            fo[fo_index_list] = grade_list[index]
-        ob_index_list = np.where(grade_list[-1] <= ob)
-        ob[ob_index_list] = grade_list[-1]
-        fo_index_list = np.where(grade_list[-1] <= fo)
-        fo[fo_index_list] = grade_list[-1]
-        ob1 = ob  # 此处需修改
-        fo1 = fo  # 此处需修改
+    return HK
 
 
-    ob2 = set(ob1)
-    fo2 = set(fo1)
-    results = list(ob2 | fo2)
-    f_mult_o_sum = 0
-    No_2_sum = 0
-
-    for result in results:
-        NO = np.sum(ob1 == result)
-        NF = np.sum(fo1 == result)
-        f_mult_o = NF * NO
-        f_mult_o_sum += f_mult_o
-        No_2_sum += NO ** 2
-    N_2 = ob_len ** 2
-    HK_score = (accuracy_score - (f_mult_o_sum / N_2)) / (1 - No_2_sum / N_2)
-    return HK_score
