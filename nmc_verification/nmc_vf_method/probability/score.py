dc61fda7f74f530928d78dee317927444cc8ef1a
liucouhua <liucouhua@163.com>
2019-12-02 22:46:50 +0800

    改进了概率检验评分

--- nmc_verification/nmc_vf_method/probability/score.py
+++ nmc_verification/nmc_vf_method/probability/score.py
@@ -1,128 +1,98 @@
-'''
 
-改过     未提交
-
-'''
 import numpy as np
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import brier_score_loss
+from nmc_verification.nmc_vf_base.tool.math_tools import mean_iteration,ss_iteration
 
-
-def accuracy(ob, fo, grade_list=None):
+def brier_score(Ob,Fo):
     '''
-    accuracy 求多分类预报准确率
-    :param ob: 实况数据 一维numpy
-    :param fo:  预测数据 一维numpy
-    :param grade_list:等级
+
+    :param Ob: 输入的概率化实况，多维的numpy，发生了则取值为1，未发生则取值为0
+    :param Fo: 预报的概率值，多维的numpy
+    :return: 实数形式的评分值
+    '''
+    bs0 =brier_score_loss(Ob,Fo)
+    return bs0
+
+def brier_score_cemmss_list(cemmss_list):
+    ce_array = np.array(cemmss_list)
+    ce_array_sum = np.sum(ce_array, axis=0)
+    total_count = ce_array_sum[0]
+    e2_sum = ce_array_sum[1]
+    brier = e2_sum / total_count
+    return brier
+
+def cemmss(Ob,Fo):
+    count = Ob.size
+    mx= np.mean(Ob)
+    sxx = np.mean(np.power(Ob - mx, 2))
+    my = np.mean(Fo)
+    syy = np.mean(np.power(Fo - my, 2))
+    error2 = np.sum(np.power(Fo - Ob,2))
+    return count,error2,mx,my,sxx,syy
+
+def bss(Ob,Fo):
+    '''
+    :param Ob: 输入的概率化实况，多维的numpy，发生了则取值为1，未发生则取值为0
+    :param Fo: 预报的概率值，多维的numpy
+    :return: 实数形式的评分值
+    '''
+    p_climate = np.sum(Ob)/Ob.size
+    Fo_climate = np.ones_like(Ob) * p_climate
+    bs0 = brier_score(Ob,Fo)
+    bs_climate = brier_score(Ob,Fo_climate)
+    if bs_climate !=0:
+        bss0 = 1 - bs0/bs_climate
+    else:
+        if bs0 ==0:
+            bss0 = 1
+        else:
+            bss0 = -999
+    return bss0
+
+def bss_cemmss(cemmss_list):
+    bs0 = brier_score_cemmss_list(cemmss_list)
+    cemmss = cemmss_list[0]
+    count_total = cemmss[0]
+    mx_total = cemmss[1]
+    my_total = cemmss[2]
+    sxx_total = cemmss[3]
+
+    for i in range(len(cemmss_list) - 1):
+        cmmsss1 = cemmss_list[i + 1]
+        count_new = cmmsss1[0]
+        mx_new = cmmsss1[1]
+        my_new = cmmsss1[2]
+        sxx_new = cmmsss1[3]
+        count_total,my_total = mean_iteration(count_total,my_total,count_new,my_new)
+        count_total, mx_total, sxx_total = ss_iteration(count_total, mx_total, sxx_total, count_new, mx_new, sxx_new)
+
+    bs_climate = sxx_total * count_total + (mx_total, - my_total)*(mx_total - my_total)
+    if bs_climate !=0:
+        bss0 = 1 - bs0/bs_climate
+    else:
+        if bs0 ==0:
+            bss0 = 1
+        else:
+            bss0 = -999
+    return bss0
+
+def roc_auc(Ob,Fo):
+    '''
+    :param Ob: 输入的概率化实况，多维的numpy，发生了则取值为1，未发生则取值为0
+    :param Fo: 预报的概率值，多维的numpy
+    :return: 实数形式的评分值
+    '''
+    return roc_auc_score(Ob,Fo)
+
+    return
+
+def RPS(ob_probablility,fo_probali):
+    '''
+    RPS用于检验多分类的概率预报和观测的一致性
+    :param ob_probablility: 二维数组，其中行数为样本数，列数为待预测的类别数
+    ，第i,j个元素表示第i个观测样本预测第j个类别是否发生，是记为1，否记为0
+    :param fo_probali: 二维数组，其中行数为样本数，列数为待预测的类别数，第i,j个元素表示第i个预报样本预测第j个类别的发生概率
     :return:
     '''
-    # 多分类预报准确率
-    if grade_list is None:
-        # ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-
-        ob1 = ob.reshape((-1))
-        fo1 = fo.reshape((-1))
-    else:
-        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
-        for index in range(len(grade_list) - 1):
-            ob_index_list = np.where((grade_list[index] <= ob)and(ob < grade_list[index + 1]))
-            ob[ob_index_list] = grade_list[index]
-            fo_index_list = np.where((grade_list[index] <= fo) and(fo< grade_list[index + 1]))
-            fo[fo_index_list] = grade_list[index]
-        ob1 = ob  # 此处需修改
-        fo1 = fo  # 此处需修改
-
-    ob_len = len(ob1)
-
-    true_num = np.sum(fo1 == ob1)
-
-    accuracy_score = true_num / ob_len
-    return accuracy_score
-
-
-def hss(ob, fo, grade_list=None):
-    '''
-    hss heidke技能得分
-    :param ob: 实况数据 一维numpy
-    :param fo: 预测数据 一维numpy
-    :param grade_list: 等级
-    :return:
-    '''
-    # 参考accuracy 做扩展修改
-
-    accuracy_score = accuracy(ob, fo, grade_list)
-    if grade_list is None:
-        # ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-
-        ob1 = ob.reshape((-1))
-        fo1 = fo.reshape((-1))
-    else:
-        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
-        for index in range(len(grade_list) - 1):
-            ob_index_list = np.where((grade_list[index] <= ob)and (ob < grade_list[index + 1]))
-            ob[ob_index_list] = grade_list[index]
-            fo_index_list = np.where((grade_list[index] <= fo)and (fo< grade_list[index + 1]))
-            fo[fo_index_list] = grade_list[index]
-        ob1 = ob  # 此处需修改
-        fo1 = fo  # 此处需修改
-
-
-    ob2 = set(ob1)
-    fo2 = set(fo1)
-    ob_len = len(ob2)
-    results = list(ob2 | fo2)
-
-    f_mult_o_sum = 0
-    for result in results:
-        NF = np.sum(ob1 == result)
-        NO = np.sum(fo1 == result)
-        f_mult_o = NF * NO
-        f_mult_o_sum += f_mult_o
-    n_2 = ob_len ** 2
-    HSS = (accuracy_score - (f_mult_o_sum / n_2)) / (1 - f_mult_o_sum / n_2)
-    return HSS
-
-
-def hk(ob, fo, grade_list=None):
-    '''
-    hk Hanssen和Kuipers判别
-    :param ob: 实况数据 一维numpy
-    :param fo: 预测数据 一维numpy
-    :param grade_list: 等级
-    :return:
-    '''
-    # 多分类预报hss技巧评分
-
-    # 参考accuracy 做扩展修改
-
-    accuracy_score = accuracy(ob, fo, grade_list)
-    ob_len = len(ob)
-    if grade_list is None:
-        # ob 和fo是n×1的numpy数组，其中每个值是代表分类的类别，如果能够兼容字符型更
-
-        ob1 = ob.reshape((-1))
-        fo1 = fo.reshape((-1))
-    else:
-        # ob 和fo 是连续的变量，通过 threshold_list 将ob 和fo划分成连续的等级之后再计算等级准确性
-        for index in range(len(grade_list) - 1):
-            ob_index_list = np.where((grade_list[index] <= ob)and (ob < grade_list[index + 1]))
-            ob[ob_index_list] = grade_list[index]
-            fo_index_list = np.where((grade_list[index] <= fo)and (fo< grade_list[index + 1]))
-            fo[fo_index_list] = grade_list[index]
-        ob1 = ob  # 此处需修改
-        fo1 = fo  # 此处需修改
-
-
-    ob2 = set(ob1)
-    fo2 = set(fo1)
-    results = list(ob2 | fo2)
-    f_mult_o_sum = 0
-    No_2_sum = 0
-
-    for result in results:
-        NO = np.sum(ob1 == result)
-        NF = np.sum(fo1 == result)
-        f_mult_o = NF * NO
-        f_mult_o_sum += f_mult_o
-        No_2_sum += NO ** 2
-    N_2 = ob_len ** 2
-    HK_score = (accuracy_score - (f_mult_o_sum / N_2)) / (1 - No_2_sum / N_2)
-    return HK_score
+    pass
