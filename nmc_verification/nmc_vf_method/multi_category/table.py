a1e4efc046e792385efcf0b34c72a9d35e1def84
liucouhua <liucouhua@163.com>
2019-12-02 22:44:09 +0800

    改进了多分类表格

--- nmc_verification/nmc_vf_method/multi_category/table.py
+++ nmc_verification/nmc_vf_method/multi_category/table.py
@@ -1,13 +1,66 @@
 from sklearn.metrics import confusion_matrix
-
 import pandas as pd
 import numpy as np
 import copy
 from sklearn.linear_model import LinearRegression
 
 
-def multi_category_contingency_table(ob, fo, grade_list=None, save_path='mult_category_contingency_table.xls'):
-    # sheet_name = 'sheet'
+
+def contingency_table(ob, fo, grade_list=None, save_path=None):
+    '''
+    multi_category_contingency_table 多分类预测列联表
+    :param ob: 实况数据 一维numpy
+    :param fo: 预测数据 一维numpy
+    :param grade_list: 等级
+    :param save_path: 保存地址
+    :return: 返回一个numpy数组
+    '''
+
+    if grade_list is not None:
+        ngrade = len(grade_list) + 1
+        conf_mx = np.zeros((ngrade, ngrade))
+        gle = [-1e300]
+        gle.extend(grade_list)
+        gle.append(1e300)
+        for i in range(len(gle) - 1):
+            ob_index_list = np.where((ob >= gle[i]) & (ob < gle[i+ 1 ]))
+            fo1 = fo[ob_index_list]
+            for j in range(len(gle)- 1 ):
+                fo_index_list = np.where((fo1 >= gle[j]) & (fo1< gle[j+ 1]))
+                conf_mx[j,i] = len(fo_index_list[0])
+
+        index_list = ["<" + str(grade_list[0])]
+        for index in range(len(grade_list) - 1):
+            index_list.append("[" + str(grade_list[index]) + "," + str(grade_list[index + 1]) + ")")
+        index_list.append(">=" + str(grade_list[-1]))
+    else:
+        new_fo = copy.deepcopy(fo).flatten()
+        new_ob = copy.deepcopy(ob).flatten()
+        index_list = list(set(np.hstack((new_ob, new_fo))))
+        conf_mx = confusion_matrix(new_fo, new_ob)
+    row_sums = conf_mx.sum(axis=1, keepdims=True)
+    conf_mx = np.hstack((conf_mx, row_sums))
+    line_sums = conf_mx.sum(axis=0, keepdims=True)
+    conf_mx = np.vstack((conf_mx, line_sums))
+    index_list.append('sum')
+    if save_path is not None:
+        table_data = pd.DataFrame(conf_mx,
+                                  columns=pd.MultiIndex.from_product([['观测'], index_list]),
+                                  index=pd.MultiIndex.from_product([['预报'], index_list])
+                                  )
+        print(save_path)
+        table_data.to_excel(save_path, sheet_name='sheet1')
+    return conf_mx
+
+def frequency_table(ob,fo, grade_list=None, save_path=None):
+    '''
+    contingency_table 多分类各等级发生样本数
+    :param ob: 实况数据 任意维numpy数组
+    :param fo: 预测数据 任意维numpy数组
+    :param grade_list: 如果grade_list 不为None，则根据该等级将ob,fo划分成不同类别
+    :param save_path: 保存地址
+    :return: 返回 2*N的numpy数组
+    '''
     '''
     multi_category_contingency_table 多分类预测列联表
     :param ob: 实况数据 一维numpy
@@ -18,33 +71,36 @@ def multi_category_contingency_table(ob, fo, grade_list=None, save_path='mult_ca
     '''
 
     if grade_list is not None:
-        shape = ob.shape
-        new_ob = np.zeros(shape, dtype=np.int64)
-        new_fo = np.zeros(shape, dtype=np.int64)
+        ngrade = len(grade_list) + 1
+        conf_mx = np.zeros((2, ngrade))
+        gle = [-1e300]
+        gle.extend(grade_list)
+        gle.append(1e300)
+        for i in range(len(gle) - 1):
+            ob_index_list = np.where((ob >= gle[i]) & (ob < gle[i+ 1 ]))
+            conf_mx[0,i] = len(ob_index_list[0])
+            fo_index_list = np.where((fo >= gle[i]) & (fo < gle[i + 1]))
+            conf_mx[1, i] = len(fo_index_list[0])
+        index_list = ["<" + str(grade_list[0])]
         for index in range(len(grade_list) - 1):
-            ob_index_list = np.where((grade_list[index] <= ob) & (ob < grade_list[index + 1]))
-            new_ob[ob_index_list] = index
-            fo_index_list = np.where((grade_list[index] <= fo) & (fo < grade_list[index + 1]))
-            new_fo[fo_index_list] = index
+            index_list.append("[" + str(grade_list[index]) + "," + str(grade_list[index + 1]) + ")")
+        index_list.append(">=" + str(grade_list[-1]))
+    else:
+        new_fo = copy.deepcopy(fo).flatten()
+        new_ob = copy.deepcopy(ob).flatten()
+        index_list = list(set(np.hstack((new_ob, new_fo))))
+        conf_mx = np.zeros((2, len(index_list)))
+        for i in range(len(index_list)):
+            ob_index_list = np.where(ob == index_list[i])
+            conf_mx[0,i] = len(ob_index_list[0])
+            fo_index_list = np.where(fo == index_list[i])
+            conf_mx[1, i] = len(fo_index_list[0])
 
-        ob_index_list = np.where(grade_list[-1] <= ob)
-        new_ob[ob_index_list] = index + 1
-        fo_index_list = np.where(grade_list[-1] <= fo)
-        new_fo[fo_index_list] = index + 1
-        fo = new_fo
-        ob = new_ob
-    index = list(set(np.hstack((ob, fo))))
-    print(index)
-    conf_mx = confusion_matrix(ob, fo)
-    row_sums = conf_mx.sum(axis=1, keepdims=True)
-    conf_mx = np.hstack((conf_mx, row_sums))
-    line_sums = conf_mx.sum(axis=0, keepdims=True)
-    conf_mx = np.vstack((conf_mx, line_sums))
-    index.append('sum')
-    table_data = pd.DataFrame(conf_mx,
-                              columns=pd.MultiIndex.from_product([['fo'], index]),
-                              index=pd.MultiIndex.from_product([['ob'], index])
-                              )
-    print(save_path)
-    table_data.to_excel(save_path, sheet_name='sheet2')
-#
+    if save_path is not None:
+        table_data = pd.DataFrame(conf_mx,
+                                  columns=pd.MultiIndex.from_product([['类别'], index_list]),
+                                  index=pd.MultiIndex.from_product([['ob-fo'], ["观测","预报"]])
+                                  )
+        print(save_path)
+        table_data.to_excel(save_path, sheet_name='sheet1')
+    return conf_mx
