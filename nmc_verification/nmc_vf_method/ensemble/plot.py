6040859004f9276db0780195a445dd755960b4d9
liucouhua <liucouhua@163.com>
2019-12-02 22:42:49 +0800

    增加了集合rank图

--- nmc_verification/nmc_vf_method/ensemble/plot.py
+++ nmc_verification/nmc_vf_method/ensemble/plot.py
@@ -1,70 +1,82 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 import numpy as np
+import copy
 
 
-def box_plot(observed, forecast, save_path=None, x_lable='observation', y_lable='forecast', title='box-plot'):
+def box_plot(ob, fo, save_path=None):
     '''
     box_plot 画一两组数据的箱型图
     ---------------
-    :param observed:实况数据 一维的numpy
-    :param forecast:预测数据 二
-
-    、维的numpy
-    :param save_path: 保存数据的路径
-    :param x_lable: 横坐标的标签
-    :param y_lable:纵坐标标签
-    :param title: 图片名字
-    :return:
+    :param ob:实况数据 一维的numpy
+    :param fo:预测数据 二维的numpy数组
+    :param save_path 不为None时输出到图片中
+    :return: 无
     '''
-    plt.boxplot((observed, forecast), labels=[x_lable, y_lable])
-    plt.title(title)
+    en_num = fo.shape[1]
+    width = en_num * 0.22 + 0.3
+    data = np.zeros((len(ob),en_num+1))
+    data[:,0] = ob[:]
+    data[:,1:] = fo[:,:]
+
+    fig = plt.figure(figsize=(width,4))
+    #plt.boxplot((observed, forecast), labels=["观测","预报" ])
+    labels = ["观测"]
+    for i in range(en_num):
+        if i == int(en_num/2):
+            labels.append(str(i)+"\n预报")
+        else:
+            labels.append(str(i))
+    plt.boxplot((data),labels=labels)
+    plt.axvline(0.5,color = "b")
+    plt.subplots_adjust(left=0.5/width,right=1-0.1/width)
+
+
     if save_path is None:
         plt.show()
     else:
         plt.savefig(save_path)
 
 
-def frequency_histogram(ob, fo, clevs, x_lable='frequency',
-                        y_lable='range', left_label='Obs', right_label='Pred',
-                        left_color='r', right_color='b', legend_location="upper right", width=0.2):
+def rank_histogram(ob,fo,save_path= None):
     '''
-    frequency_histogram 对比测试数据和实况数据的发生的频率
-    :param ob:
-    :param fo:
-    :param clevs:
-    :param x_lable:
-    :param y_lable:
-    :param left_label:
-    :param right_label:
-    :param left_color:
-    :param right_color:
-    :param legend_location:
-    :param width:
+    :param ob:一维numpy数组
+    :param fo: 二维numpy数组
+    :param save_path:
     :return:
     '''
-    p_ob = []
-    p_fo = []
-    x = np.arange(6)
-    xticklabels = []
-    for i in range(0, len(clevs) - 1):
-        index0 = np.where((ob >= clevs[i]) & (ob < clevs[i + 1]))
-        xticklabels.append(str(clevs[i]) + '-' + str(clevs[i + 1]))
-        p_ob.append(len(index0[0]) / len(ob))
-        index0 = np.where((fo >= clevs[i]) & (fo < clevs[i + 1]))
-        p_fo.append(len(index0[0]) / len(fo))
-    index0 = np.where(ob >= clevs[-1])
-    p_ob.append(len(index0[0]) / len(ob))
-    index0 = np.where(fo >= clevs[-1])
-    p_fo.append(len(index0[0]) / len(fo))
-    xticklabels.append('>=' + str(clevs[-1]))
-    ax3 = plt.axes()
-    ax3.bar(x + 0.25, p_ob, width=width, facecolor=left_color, label=left_label)
-    ax3.bar(x - 0.05, p_fo, width=width, facecolor=right_color, label=right_label)
-    ax3.legend(loc=legend_location)
-    ax3.set_xlabel(x_lable, fontsize=10)
-    ax3.set_xticks(x)
-    ax3.set_xticklabels(xticklabels, fontsize=9)
-    ax3.set_ylabel(y_lable, fontsize=10)
-    ax3.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(100))
-    plt.show()
+    en_num = fo.shape[1]
+    sample_num = ob.size
+    fo1 = copy.deepcopy(fo)
+    fo1.sort(axis = 1)
+    index = np.where(ob<fo1[:,0])
+    rank_num = [len(index[0])]
+    for i in range(en_num-1):
+        index = np.where((ob>=fo1[:,i]) & (ob < fo1[:,i+1]))
+        rank_num.append(len(index[0]))
+    index = np.where(ob>=fo1[:,-1])
+    rank_num.append(len(index[0]))
+    rank_rate = np.array(rank_num)/sample_num
+    x = np.arange(0,en_num+1)
+    ymax = np.max(rank_rate) * 1.5
+    plt.bar(x,rank_rate)
+    plt.ylabel("比例")
+    plt.xlabel("观测值在集合序列中的排序")
+    plt.title("Rank Histogram")
+    plt.ylim(0,ymax)
+    if save_path is None:
+        plt.show()
+    else:
+        plt.savefig(save_path)
+    pass
+
+
+
+fo = np.random.rand(100,50)
+ob = np.random.rand(100)
+
+
+pass
+#mse_variance(ob,fo)
