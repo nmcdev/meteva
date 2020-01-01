1ad37cf1d82e7cf5e497f6f1957f4c521b0419ae
liucouhua <liucouhua@163.com>
2020-01-01 22:52:53 +0800

    修改了根据中间结果计算的方式

--- nmc_verification/nmc_vf_method/continuous/score.py
+++ nmc_verification/nmc_vf_method/continuous/score.py
@@ -1,196 +1,175 @@
 import numpy as np
 from nmc_verification.nmc_vf_base.tool.math_tools import mean_iteration,sxy_iteration,ss_iteration
+from nmc_verification.nmc_vf_base import IV
 
 def tase(Ob,Fo):
     '''
     计算平均误差、平均绝对误差、均方误差、均方根误差的中间结果
     -----------------------------
-    :param Ob: 实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return: python 元组
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: 一维numpy数组，其内容依次为总样本数、误差总和、绝对误差总和、误差平方总和
     '''
     total_count = Ob.size
-    e_sum = np.sum(Ob - Fo)
-    ae_sum = np.sum(np.abs(Ob - Fo))
-    se_sum = np.sum(np.square(Ob - Fo))
-    return total_count,e_sum,ae_sum,se_sum
+    e_sum = np.sum(Fo - Ob)
+    ae_sum = np.sum(np.abs(Fo - Ob))
+    se_sum = np.sum(np.square(Fo - Ob))
+    return np.array([total_count,e_sum,ae_sum,se_sum])
+
 def me(Ob, Fo):
     '''
     me 求两组数据的误差平均值
     -----------------------------
-    :param Ob: 实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return: 实数，最优值为0
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: 负无穷到正无穷的实数，最优值为0
     '''
     mean_error = np.mean(Fo - Ob)
     return mean_error
-def me_tase(tase_list):
+
+def me_tase(tase_array):
     '''
     me 求两组数据的误差平均值
-    :param tase_list,一个组元的列表，其中每个组元为(total_count,e_sum,ae_sum,se_sum),分别代表由部分数据计算得到的
+    :param tase_array:包含命中空报和漏报的多维数组，其中最后一维长度为4，分别记录了（total_count,e_sum,ae_sum,se_sum）
     （样本数，误差和、绝对误差和，误差平方和），它由tase返回
-    :return: 0到1的实数，完美值为1
+    :return: 负无穷到正无穷的实数，最优值为0
     '''
-    tase_array = np.array(tase_list)
-    tase_array_sum = np.sum(tase_array, axis=0)
-    total_count = tase_array_sum[0]
-    e_sum = tase_array_sum[1]
-    mean_error = e_sum/total_count
+    mean_error = tase_array[...,1]/tase_array[...,0]
     return mean_error
+
 def mae(Ob, Fo):
     '''
-    mae对两组数据求平均绝对值误差
+    mean_abs_error,对两组数据求平均绝对值误差
     -----------------------
-    :param Ob:实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return: mean_abs_error
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: 0到无穷大，最优值为0
     '''
     mean_abs_error = np.mean(np.abs(Ob - Fo))
     return mean_abs_error
-def mae_tase(tase_list):
+def mae_tase(tase_array):
     '''
-    mae 求两组数据的平均绝对误差
-    :param tase_list,一个组元的列表，其中每个组元为(total_count,e_sum,ae_sum,se_sum),分别代表由部分数据计算得到的
+    mean_abs_error,求两组数据的平均绝对误差
+    :param tase_array:包含命中空报和漏报的多维数组，其中最后一维长度为4，分别记录了（total_count,e_sum,ae_sum,se_sum）
     （样本数，误差和、绝对误差和，误差平方和），它由tase返回
-    :return: 0到1的实数，完美值为1
+    :return: 0到无穷大，最优值为0
     '''
-    tase_array = np.array(tase_list)
-    tase_array_sum = np.sum(tase_array, axis=0)
-    total_count = tase_array_sum[0]
-    ae_sum = tase_array_sum[2]
-    mean_abs_error = ae_sum/total_count
+    mean_abs_error = tase_array[...,2]/tase_array[...,0]
     return mean_abs_error
+
 def mse(Ob, Fo):
     '''
-    mse  求两组数据的均方误差
+    mean_sqrt_error, 求两组数据的均方误差
     ----------------------------------
-    :param Ob:实况数据 多维numpy
-    :param Fo:预测数据 多维numpy
-    :return: mean_sqrt_error
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: 0到无穷大，最优值为0
     '''
-    mean_sqrt_error = np.mean(np.square(Ob - Fo))
-    return mean_sqrt_error
-def mse_tase(tase_list):
+
+    mean_squre_error = np.mean(np.square(Ob - Fo))
+    return mean_squre_error
+def mse_tase(tase_array):
     '''
     mse 求两组数据的均方误差
-    :param tase_list,一个组元的列表，其中每个组元为(total_count,e_sum,ae_sum,se_sum),分别代表由部分数据计算得到的
+    :param tase_array:包含命中空报和漏报的多维数组，其中最后一维长度为4，分别记录了（total_count,e_sum,ae_sum,se_sum）
     （样本数，误差和、绝对误差和，误差平方和），它由tase返回
-    :return: 0到1的实数，完美值为1
+    :return: 0到无穷大，最优值为0
     '''
-    tase_array = np.array(tase_list)
-    tase_array_sum = np.sum(tase_array, axis=0)
-    total_count = tase_array_sum[0]
-    se_sum = tase_array_sum[3]
-    mean_squre_error = se_sum/total_count
+    mean_squre_error = tase_array[...,3]/tase_array[...,0]
     return mean_squre_error
+
 def rmse(Ob, Fo):
     '''
-    rmse 求两组数据的均方根误差
+    root_mean_square_error 求两组数据的均方根误差
     ------------------------------
-    :param Ob:实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return:mean_sqrt_error
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: 0到无穷大，最优值为0
     '''
     mean_sqrt_error = np.sqrt(np.mean(np.square(Ob - Fo)))
     return mean_sqrt_error
-def rmse_tase(tase_list):
+def rmse_tase(tase_array):
     '''
     mse 求两组数据的均方根误差
-    :param tase_list,一个组元的列表，其中每个组元为(total_count,e_sum,ae_sum,se_sum),分别代表由部分数据计算得到的
+    :param tase_array:包含命中空报和漏报的多维数组，其中最后一维长度为4，分别记录了（total_count,e_sum,ae_sum,se_sum）
     （样本数，误差和、绝对误差和，误差平方和），它由tase返回
-    :return: 0到1的实数，完美值为1
+    :return: 0到无穷大，最优值为0
     '''
-    tase_array = np.array(tase_list)
-    tase_array_sum = np.sum(tase_array, axis=0)
-    total_count = tase_array_sum[0]
-    se_sum = tase_array_sum[3]
-    mean_squre_error = se_sum/total_count
-    rmse = np.sqrt(mean_squre_error)
-    return rmse
-def me_mae_rmse(Ob,Fo):
-    '''
-    同时计算平均误差、平均绝对误差、均方根误差
-    :param Ob: 实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return: python 元组
-    '''
-    mean_error = me(Ob,Fo)
-    mean_abs_error = mae(Ob,Fo)
-    rmse1 = rmse(Ob,Fo)
-    return mean_error,mean_abs_error,rmse1
-def me_mae_rmse_tase(tase_list):
-    '''
-    同时计算平均误差、平均绝对误差、均方根误差
-    :param tase_list,一个组元的列表，其中每个组元为(total_count,e_sum,ae_sum,se_sum),分别代表由部分数据计算得到的
-    （样本数，误差和、绝对误差和，误差平方和），它由tase返回
-    :return: python 元组
-    '''
-    tase_array = np.array(tase_list)
-    tase_array_sum = np.sum(tase_array, axis=0)
-    total_count = tase_array_sum[0]
-    e_sum = tase_array_sum[1]
-    mean_error = e_sum/total_count
-    ae_sum = tase_array_sum[2]
-    mean_abs_error = ae_sum/total_count
-    se_sum = tase_array_sum[3]
-    mean_squre_error = se_sum/total_count
-    rmse = np.sqrt(mean_squre_error)
-    return mean_error,mean_abs_error,rmse
+    root_mean_sqrt_error = np.sqrt(tase_array[...,3]/tase_array[...,0])
+    return root_mean_sqrt_error
+
 def bias(Ob, Fo):
     '''
-    bias 求预测数据和实况数据的平均值的比
+    均值偏差 求预测数据和实况数据的平均值的比
     ------------------------------
-    :param Ob:实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return:  bias0
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return:  0到正无穷，最优值为1
     '''
-    bias0 = np.mean(Fo) / (np.mean(Ob) + 1e-6)
+    mean_ob = np.mean(Ob)
+    if mean_ob == 0:
+        bias0 = IV
+    else:
+        bias0 = np.mean(Fo) / mean_ob
     return bias0
+
+def bias_tmmsss(tmmsss_array):
+    '''
+    均值偏差 求预测数据和实况数据的平均值的比
+    :param tmmsss_array: 包含命中空报和漏报的多维数组，其中最后一维长度为6，分别记录了（count,mx,my,sxx,syy,sxy）
+    :return:
+    '''
+    mean_ob = tmmsss_array[...,1] + 0
+    mean_fo = tmmsss_array[...,2]
+    if mean_ob.size ==1:
+        if mean_ob == 0:
+            bias0 = IV
+        else:
+            bias0 = mean_fo/mean_ob
+    else:
+        mean_ob[mean_ob == 0] = IV
+        bias0 = mean_ob/mean_fo
+        bias0[mean_ob == IV] = IV
+    return bias0
+
+
 def corr(Ob, Fo):
     '''
-    corr 求实况数据还和预测数据之间的相关系数
+    相关系数，求实况数据还和预测数据之间的相关系数
     -----------------------------
-    :param Ob: 实况数据 不定长维度的numpy
-    :param Fo: 测试数据 不定长维度的numpy
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
     :return: corr0
     '''
     ob_f = Ob.flatten()
     fo_f = Fo.flatten()
     corr0 = np.corrcoef(ob_f, fo_f)[0, 1]
     return corr0
-def corr_cmmsss(cmmsss_list):
+
+def corr_tmmsss(tmmsss_array):
     '''
-    根据中间结果列表，计算相关系数
-    :param cmmsss_list:
-    :return: -1到1的实数
+    相关系数，求实况数据还和预测数据之间的相关系数
+    :param tmmsss_array: 包含命中空报和漏报的多维数组，其中最后一维长度为6，分别记录了（count,mx,my,sxx,syy,sxy）
+    :return:
     '''
-    cmmsss = cmmsss_list[0]
-    count_total = cmmsss[0]
-    mx_total = cmmsss[1]
-    my_total = cmmsss[2]
-    sxx_total = cmmsss[3]
-    syy_total = cmmsss[4]
-    sxy_total = cmmsss[5]
-    for i in range(len(cmmsss_list)-1):
-        cmmsss1 = cmmsss_list[i+1]
-        count_new = cmmsss1[0]
-        mx_new = cmmsss1[1]
-        my_new = cmmsss1[2]
-        sxx_new = cmmsss1[3]
-        syy_new = cmmsss1[4]
-        sxy_new = cmmsss1[5]
-        _, _, sxx_total = ss_iteration(count_total,mx_total,sxx_total,count_new,mx_new,sxx_new)
-        _, _, syy_total = ss_iteration(count_total,my_total,syy_total,count_new,my_new,syy_new)
-        count_total,mx_total,my_total,sxy_total = sxy_iteration(count_total,mx_total,my_total,sxy_total,
-                                                                count_new,mx_new,my_new,sxy_new)
-    corr = sxy_total/np.sqrt(sxx_total * syy_total)
+    sxx = tmmsss_array[...,3]
+    syy = tmmsss_array[...,4]
+    sxy = tmmsss_array[...,5]
+    sxxsyy = np.sqrt(sxx * syy)
+    if sxxsyy.size == 1:
+        if sxxsyy == 0:
+            sxxsyy = 1e-10
+    else:
+        sxxsyy[sxxsyy == 0] = 1e-10
+    corr = sxy/sxxsyy
     return corr
-def cmmsss(Ob,Fo):
+
+def tmmsss(Ob,Fo):
     '''
     统计相关系数等检验量所需的中间变量
-    :param Ob: 实况数据 一维numpy
-    :param Fo:预测数据 一维numpy
-    :return: python 元组，其元素为根据Ob和Fo
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: numpy 一维数组，其元素为根据Ob和Fo
     计算出的（样本数，观测平均值，预报平均值，观测方差，预报方差，协方差
     '''
     ob_f = Ob.flatten()
@@ -203,7 +182,33 @@ def cmmsss(Ob,Fo):
     sxx = np.mean(np.power(dx,2))
     syy = np.mean(np.power(dy,2))
     sxy = np.mean(dx * dy)
-    return count,mx,my,sxx,syy,sxy
+    return np.array([count,mx,my,sxx,syy,sxy])
+
+def tmmsss_merge(tmmsss0,tmmsss1):
+    '''
+    将两份包含样本数、平均值和方差、协方差的中间结果合并
+    :param tmmsss0: 长度6的一维数组，分别记录了（count,mx,my,sxx,syy,sxy）
+    :param tmmsss1: 长度6的一维数组，分别记录了（count,mx,my,sxx,syy,sxy）
+    :return: 长度6的一维数组，分别记录了（count,mx,my,sxx,syy,sxy）
+    '''
+    count_0 = tmmsss0[0]
+    mx_0 = tmmsss0[1]
+    my_0 = tmmsss0[2]
+    sxx_0 = tmmsss0[3]
+    syy_0 = tmmsss0[4]
+    sxy_0 = tmmsss0[5]
+    count_1 = tmmsss1[0]
+    mx_1 = tmmsss1[1]
+    my_1 = tmmsss1[2]
+    sxx_1 = tmmsss1[3]
+    syy_1 = tmmsss1[4]
+    sxy_1 = tmmsss1[5]
+    _, _, sxx_total = ss_iteration(count_0, mx_0, sxx_0, count_1, mx_1, sxx_1)
+    _, _, syy_total = ss_iteration(count_0, my_0, syy_0, count_1, my_1, syy_1)
+    count_total, mx_total, my_total, sxy_total = sxy_iteration(count_0, mx_0, my_0, sxy_0,
+                                                               count_1, mx_1, my_1, sxy_1)
+    return np.array([count_total,mx_total,my_total,sxx_total,syy_total,sxy_total])
+
 def mre(Ob, Fo):
     '''
     mre  精细化网格预报竞赛检验办法中的降水量定量相对误差检验指标
@@ -218,22 +223,79 @@ def mre(Ob, Fo):
         d = Ob - Fo
         s1 = s[s > 0]
         d1 = d[s > 0]
-
         are0 = np.mean(np.abs(d1 / s1))
         return are0
+
+def mre_toar(toar_array):
+    '''
+    mre  精细化网格预报竞赛检验办法中的降水量定量相对误差检验指标
+    :param toar_array: 包含命中空报和漏报的多维数组，其中最后一维长度为2，分别记录了（预报和观测值之和大于0样本数,各点相对误差绝对值总和）
+    （预报和观测值之和大于0样本数、各点相对误差绝对值总和），它由toar返回
+    :return:
+    '''
+    count = toar_array[...,0] + 0
+    if count.size ==1:
+        if count == 0:
+            mre0 = IV
+        else:
+            mre0 = toar_array[...,1] / count
+    else:
+        count[count<0] = 1e-10
+        ar = toar_array[...,1]
+        mre0 = ar/count
+        mre0[count<1] = IV
+    return mre0
+
+def toar(Ob,Fo):
+    '''
+    相对误差检验指标的中间结果量
+    :param Ob: 实况数据  任意维numpy数组
+    :param Fo: 预测数据 任意维numpy数组,Fo.shape 和Ob.shape一致
+    :return: 一维numpy数组，其内容依次为预报和观测值之和大于0样本数、各点相对误差绝对值总和
+    '''
+    s = Ob + Fo
+    d = Ob - Fo
+    s1 = s[s > 0]
+    d1 = d[s > 0]
+    ar = np.sum(np.abs(d1 / s1))
+    return np.array([s1.size,ar])
+
+
 def nse(Ob,Fo):
     '''
-    nse纳什系数, 常用于计算两个非正态序列的相对误差情况，取值范围为负无穷至1，其取值越接近1代表越准确
+    nse纳什系数, 常用于计算两个非正态序列的相对误差情况，
     :param Ob:实况数据 不定长维度的numpy
     :param Fo:测试数据 不定长维度的numpy
-    :return:
+    :return:负无穷至1，最优值为1
     '''
     mob = np.mean(Ob)
     qdob = np.mean(np.power(Ob - mob,2))
     if qdob ==0:
-        return -9999
+        return IV
     else:
         return 1-np.mean(np.power(Ob - Fo,2))/qdob
+
+def nse_tase_tmmsss(tase_array,tmmsss_array):
+    '''
+
+    :param tase_array:
+    :param tmmsss_array:
+    :return:
+    '''
+    sxx = tmmsss_array[...,3] + 0
+    if sxx.size == 1:
+        if sxx == 0:
+            nse0 = IV
+        else:
+            nse0 = 1 - tase_array[...,3]/tase_array[...,0]/sxx
+    else:
+        sum = sxx + 0
+        sum[sxx ==0] = 1e-10
+        mse0 = tase_array[...,3]/tase_array[...,0]
+        nse0 = 1 - mse0/sum
+        nse0[sxx == 0] = IV
+    return nse0
+
 # FSS
 def FSS(Ob, Fo, window_sizes_list=[3], threshold_list=[50], Masker=None):
     '''
