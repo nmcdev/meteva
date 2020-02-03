1647df18a61c4a299d8239e671699ffd24a4f09a
liucouhua <liucouhua@163.com>
2020-02-03 14:48:47 +0800

    索引具体化

--- nmc_verification/nmc_vf_method/probability/__init__.py
+++ nmc_verification/nmc_vf_method/probability/__init__.py
@@ -1,7 +1,6 @@
-
 from . import score
 from . import table
 from . import plot
-from .score import *
+from .score import tems,tems_merge,bs,bs_tems,bss_tems,bss,roc_auc_score,roc_auc_hnh
 from .table import *
-from .plot import *
+from .plot import hnh,pod_hfmc,pofd_hfmc,reliability_hnh,discrimination_hnh,roc_hfmc,roc_hnh,comprehensive_hnh,comprehensive_probability,roc
