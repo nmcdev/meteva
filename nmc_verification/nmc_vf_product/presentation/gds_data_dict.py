df46208906863215dbb6ca41bb4f34d7d2976bb6
liucouhua <liucouhua@163.com>
2020-02-03 15:05:29 +0800

    表现层的数据字典

--- nmc_verification/nmc_vf_product/presentation/gds_data_dict.py
+++ nmc_verification/nmc_vf_product/presentation/gds_data_dict.py
@@ -1,9 +1,11 @@
 
 fo_grd_dir_dict = {
     "ECMWF" :{
-        "temp" :{
-            "2M" :"ECMWF_HR/UGRD/TMP_2M/YYMMDDHH.TTT",
-            "800hPa" :"ECMWF_HR/UGRD/TMP/800/YYMMDDHH.TTT"
+        "温度" :{
+            "2M" :"ECMWF_HR/TMP_2M/YYMMDDHH.TTT",
+            "800hPa" :"ECMWF_HR/TMP/800/YYMMDDHH.TTT",
+            "700hPa" :"ECMWF_HR/TMP/700/YYMMDDHH.TTT",
+            "850hPa" :"ECMWF_HR/TMP/850/YYMMDDHH.TTT",
         },
         "U" :{
             "10M" :r"ECMWF_HR/UGRD_10M/YYMMDDHH.TTT",
@@ -16,22 +18,22 @@ fo_grd_dir_dict = {
             "100M": r"ECMWF_HR/VGRD_100M/YYMMDDHH.TTT",
             "800hPa": r"ECMWF_HR/VGRD/800/YYMMDDHH.TTT",
             "700hPa": r"ECMWF_HR/VGRD/700/YYMMDDHH.TTT"
+        },
+        "能见度":{
+            "2M" :r"ECMWF_HR/VIS/YYMMDDHH.TTT",
+        },
+        "相对湿度":{
+            "800hPa": "ECMWF_HR/RH/800/YYMMDDHH.TTT",
+            "700hPa": "ECMWF_HR/RH/700/YYMMDDHH.TTT",
+            "850hPa": "ECMWF_HR/RH/850/YYMMDDHH.TTT",
         }
+
     }
 }
 model_update_hours_dict = {
-    "ECMWF":[8,20]
+    "ECMWF":[8,20],
+    "GRAPES_GFS":[8,20],
+    "NCEP":[8,20],
 }
-id_dict = {
-    "竞赛1号站" :651801,
-}
-value_type_dict = {
-    "平均风_2分钟": "wind"
-}
-ob_s_name_dict = {
-    "平均风_2分钟": "平均风速_2分钟",
-}
-ob_d_name_dict = {
-    "平均风_2分钟": "平均风向_2分钟",
-}
+
 
