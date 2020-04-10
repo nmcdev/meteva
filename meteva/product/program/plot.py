03667d4e22e036339b6c8bb4d532c51e1d38f5e9
liucouhua <liucouhua@163.com>
2020-04-10 08:56:05 +0800

    参数规范化变更
    为了使得函数库的界面更加规范，将group 和select的功能独立出来，所有的函数尽量包含 s, g, gll, group_name_list 等几个参数

--- meteva/product/program/plot.py
+++ meteva/product/program/plot.py
@@ -1,16 +1,23 @@
 from meteva.base.fun import *
 from meteva.product.program.fun import *
 
-def plot(sta_ob_and_fos,method,group_by = None,group_list_list = None,save_dir = None,title = None,para1 = None):
+def plot(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,title = None,para1 = None):
 
-    discription_uni = get_unique_coods(sta_ob_and_fos)
-    sta_ob_and_fos_list,group_list_list1 = group(sta_ob_and_fos,group_by,group_list_list)
+    if s is not None:
+        if g is not None:
+            if g == "last_range" or g == "last_step":
+                s["drop_last"] = False
+            else:
+                s["drop_last"] = True
+    sta_ob_and_fos = sele_by_dict(sta_ob_and_fos0, s)
+    #discription_uni = get_unique_coods(sta_ob_and_fos)
+    sta_ob_and_fos_list,gll1 = group(sta_ob_and_fos,g,gll)
     data_name = meteva.base.get_stadata_names(sta_ob_and_fos)
     fo_num = len(data_name) -1
-    ensemble_score_method = [meteva.method.box_plot_ensemble]
+    ensemble_score_method = [meteva.method.box_plot_ensemble,meteva.method.rank_histogram]
     group_num = len(sta_ob_and_fos_list)
 
-    valid_group_list_list = []
+    valid_gll = []
     if type(method) == str:
         method =  globals().get(method)
     if method in ensemble_score_method:
@@ -20,19 +27,22 @@ def plot(sta_ob_and_fos,method,group_by = None,group_list_list = None,save_dir =
                 pass
             else:
                 valid_group_list = None
-                if group_list_list1 is None:
-                    valid_group_list_list = None
+                if gll1 is None:
+                    valid_gll = None
                 else:
-                    valid_group_list = group_list_list1[i]
-                    valid_group_list_list.append(group_list_list1[i])
+                    valid_group_list = gll1[i]
+                    valid_gll.append(gll1[i])
                 ob = sta[data_name[0]].values
                 fo = sta[data_name[1:]].values
-                save_path = get_save_path(save_dir,method,group_by,valid_group_list,"",".png",title)
-                title1 = get_title(method,group_by,valid_group_list,"",title,discription_uni)
+                save_path = get_save_path(save_dir,method,g,valid_group_list,"",".png",title)
+                #title1 = get_title(method,g,valid_group_list,"",title,discription_uni)
+
+                title1 = meteva.product.program.get_title_from_dict(method, s, g, valid_group_list,
+                                                                    None)
                 if para1 is None:
-                    method(ob, fo,save_path = save_path,title = title1,member_list = data_name[1:])
+                    method(ob, fo,save_path = save_path,title = title1)
                 else:
-                    method(ob, fo,para1,save_path = save_path,title = title1,member_list = data_name[1:])
+                    method(ob, fo,para1,save_path = save_path,title = title1)
     else:
         for i in range(group_num):
             sta = sta_ob_and_fos_list[i]
@@ -40,21 +50,23 @@ def plot(sta_ob_and_fos,method,group_by = None,group_list_list = None,save_dir =
                 pass
             else:
                 valid_group_list = None
-                if group_list_list1 is None:
-                    valid_group_list_list = None
+                if gll1 is None:
+                    valid_gll = None
                 else:
-                    valid_group_list = group_list_list1[i]
-                    valid_group_list_list.append(group_list_list1[i])
+                    valid_group_list = gll1[i]
+                    valid_gll.append(gll1[i])
                 ob = sta[data_name[0]].values
 
                 for j in range(fo_num):
                     fo = sta[data_name[j+1]].values
-                    save_path = get_save_path(save_dir,method,group_by,valid_group_list,data_name[j+1],".png",title)
-                    title1 = get_title(method,group_by,valid_group_list,data_name[j+1],title,discription_uni)
+                    save_path = get_save_path(save_dir,method,g,valid_group_list,data_name[j+1],".png",title)
+                    #title1 = get_title(method,g,valid_group_list,data_name[j+1],title,discription_uni)
+                    title1 = meteva.product.program.get_title_from_dict(method, s, g, valid_group_list,data_name[j+1])
+
                     if para1 is None:
                         method(ob, fo,save_path = save_path,title = title1)
                     else:
                         method(ob, fo,para1,save_path = save_path,title = title1)
 
 
-    return valid_group_list_list
+    return valid_gll
--- meteva/product/program/score.py
+++ meteva/product/program/score.py
@@ -1,20 +1,27 @@
 from meteva.base import *
 from meteva.method import *
 from meteva.product.program.fun import *
-
+import pandas as pd
 import numpy as np
 
 
+def score(sta_ob_and_fos0,method,s = None,g = None,gll = None,para1 = None,para2 = None):
 
+    if s is not None:
+        if g is not None:
+            if g == "last_range" or g == "last_step":
+                s["drop_last"] = False
+            else:
+                s["drop_last"] = True
+    sta_ob_and_fos = sele_by_dict(sta_ob_and_fos0, s)
 
-def score(sta_ob_and_fos,method,group_by = None,group_list_list = None,para1 = None,para2 = None):
     if type(method) == str:
         method =  globals().get(method)
     if method == meteva.method.FSS_time:
-        if group_by == "dtime":
+        if g == "dtime":
             print("FSS_time 检验时，参数group_by不能选择dtime")
             return
-    sta_ob_and_fos_list,group_list_list1 = group(sta_ob_and_fos,group_by,group_list_list)
+    sta_ob_and_fos_list,group_list_list1 = group(sta_ob_and_fos,g,gll)
     data_name = meteva.base.get_stadata_names(sta_ob_and_fos)
     fo_num = len(data_name) -1
     ensemble_score_method = [meteva.method.cr]
@@ -24,21 +31,9 @@ def score(sta_ob_and_fos,method,group_by = None,group_list_list = None,para1 = N
     else:
         para_num = len(para1)
 
+    sta_result = None
 
-    if method in ensemble_score_method:
-        result = np.zeros((group_num,para_num))
-        for i in range(group_num):
-            sta = sta_ob_and_fos_list[i]
-            if(len(sta.index) == 0):
-                result[i,:] = meteva.base.IV
-            else:
-                ob = sta[data_name[0]].values
-                fo = sta[data_name[1:]].values
-                if para1 is None:
-                    result[i, :] = method(ob, fo)
-                else:
-                    result[i,:] = method(ob, fo,para1)
-    elif method == meteva.method.FSS_time:
+    if method == meteva.method.FSS_time:
         #统计dtime的集合
         dtime_list = list(set(sta_ob_and_fos["dtime"].values.tolist()))
         dtime_list.sort()
@@ -72,20 +67,42 @@ def score(sta_ob_and_fos,method,group_by = None,group_list_list = None,para1 = N
                 result2 = FSS_time(ob_array, fo_array, para1, para2)
                 result1.append(result2)
             result.append(result1)
-
         result = np.array(result) #将数据转换成数组
-    else:
-        if fo_num ==0:
-            result = np.zeros((group_num,para_num))
-        else:
-            result = np.zeros((group_num,fo_num,para_num))
-        for i in range(group_num):
-            #print(group_num)
-            sta = sta_ob_and_fos_list[i]
-            if(len(sta.index) == 0):
-                result[i,:] = meteva.base.IV
+        result = result.squeeze()
 
+    else:
+
+        nead_lon_lat = False
+        if g == "id" and gll is None:
+            nead_lon_lat = True
+        lon_lat_list = []
+
+        if method in ensemble_score_method:
+            result = np.zeros((group_num,para_num))
+            for i in range(group_num):
+                sta = sta_ob_and_fos_list[i]
+                #if(len(sta.index) == 0):
+                #    result[i,:] = meteva.base.IV
+                #else:
+                ob = sta[data_name[0]].values
+                fo = sta[data_name[1:]].values
+                if para1 is None:
+                    result[i, :] = method(ob, fo)
+                else:
+                    result[i,:] = method(ob, fo,para1)
+                if nead_lon_lat:
+                    lon_lat_list.append(sta.iloc[0,[4,5]])
+        else:
+            if fo_num ==0:
+                result = np.zeros((group_num,para_num))
             else:
+                result = np.zeros((group_num,fo_num,para_num))
+            for i in range(group_num):
+                #print(group_num)
+                sta = sta_ob_and_fos_list[i]
+                #if(len(sta.index) == 0):
+                #    result[i,:] = meteva.base.IV
+                #else:
                 ob = sta[data_name[0]].values
                 if fo_num>0:
                     for j in range(fo_num):
@@ -99,9 +116,40 @@ def score(sta_ob_and_fos,method,group_by = None,group_list_list = None,para1 = N
                         result[i] = method(ob, None)
                     else:
                         result[i] = method(ob, None,para1)
+                if nead_lon_lat:
+                    lon_lat_list.append(sta.iloc[0,[4,5]])
+        result = result.squeeze()
+        if nead_lon_lat:
+            df = pd.DataFrame(lon_lat_list)
+            df["id"] = group_list_list1
+            df["level"] = np.NAN
+            df["time"] = np.NAN
+            df["dtime"] = np.NAN
+            if fo_num ==0:
+                if para1 is None:
+                    df["ob"] = result
+                else:
+                    if isinstance(para1,list):
+                        for i in range(len(para1)):
+                            para = para1[i]
+                            df[para] = result[:,i]
+                    else:
+                        df[para1] = result
+            else:
+                result = result.reshape(group_num,fo_num,para_num)
+                for j in range(fo_num):
+                    if para1 is None:
+                        df[data_name[1+j]] = result[:,j,0]
+                    else:
+                        if isinstance(para1, list):
+                            for i in range(len(para1)):
+                                para = para1[i]
+                                df[data_name[1+j]+"_"+str(para)] = result[:,j, i]
+                        else:
+                            df[data_name[1 + j]] = result[:,j,0]
 
-    result = result.squeeze()
-    return result,group_list_list1
+            sta_result = sta_data(df)
+    return result,group_list_list1,sta_result
 
 
 '''
