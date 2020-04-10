9d65059b4aab8941b279600d61ddca426122c6d4
liucouhua <liucouhua@163.com>
2020-04-10 08:50:00 +0800

    增加了分组函数

--- meteva/base/fun/grouping.py
+++ meteva/base/fun/grouping.py
@@ -3,30 +3,32 @@ import datetime
 import pandas as pd
 import numpy as np
 
-def group(sta_ob_and_fos,group_by,group_list_list = None):
+
+def group(sta_ob_and_fos,g = None,gll = None):
     valid_group_list_list = []
     sta_ob_and_fos_list = []
-    if group_by is None:
+    if g is None:
         sta_ob_and_fos_list.append(sta_ob_and_fos)
     else:
-        if group_list_list is not None:
+        group_list_list = gll
+        if gll is not None:
             group_list_list0 = []
-            for group_list in group_list_list:
+            for group_list in gll:
                 if isinstance(group_list,list):
                     group_list_list0.append(group_list)
                 else:
                     group_list_list0.append([group_list])
             group_list_list = group_list_list0
-        valid_group = ["level","time","year","month","day","dayofyear","hour",
-                       "ob_time","ob_year","ob_month","ob_day","ob_dayofyear","ob_hour",
-                       "dtime","dday","dhour","id"]
-        if not group_by in valid_group:
+        valid_group = ["level","time","time_range","year","month","day","dayofyear","hour",
+                       "ob_time","ob_time_range","ob_year","ob_month","ob_day","ob_dayofyear","ob_hour",
+                       "dtime","dtime_range","dday","dhour","id","lon_range","lon_step","lat_range","lat_step","last_range","last_step","grid"]
+        if not g in valid_group:
             print("group_by 参数必须为如下列表中的选项：")
             print(str(valid_group))
             return None
-        if group_by == "level":
+        if g == "level":
             if group_list_list is None:
-                grouped_dict = dict(list(sta_ob_and_fos.groupby(group_by)))
+                grouped_dict = dict(list(sta_ob_and_fos.groupby(g)))
                 keys = grouped_dict.keys()
                 for key in keys:
                     valid_group_list_list.append([key])
@@ -37,9 +39,9 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "time":
+        elif g == "time":
             if group_list_list is None:
-                grouped_dict = dict(list(sta_ob_and_fos.groupby(group_by)))
+                grouped_dict = dict(list(sta_ob_and_fos.groupby(g)))
                 keys = grouped_dict.keys()
                 for key in keys:
                     valid_group_list_list.append([key])
@@ -51,7 +53,18 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "year":
+        elif g == "time_range":
+            if group_list_list is None:
+                print("当group_by = time_range时 group_list_list 参数不能为None")
+            else:
+                for group_list in group_list_list:
+                    sta = meteva.base.between_time_range(sta_ob_and_fos,group_list[0],group_list[1])
+                    if len(sta.index) !=0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+
+        elif g == "year":
             fo_times = pd.Series(0, index=sta_ob_and_fos['time'])
             if group_list_list is None:
                 grouped_dict = dict(list(sta_ob_and_fos.groupby(fo_times.index.year)))
@@ -66,7 +79,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "month":
+        elif g == "month":
             fo_times = pd.Series(0, index=sta_ob_and_fos['time'])
             if group_list_list is None:
                 grouped_dict = dict(list(sta_ob_and_fos.groupby(fo_times.index.month)))
@@ -80,7 +93,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "day":
+        elif g == "day":
             time0 = datetime.datetime(1900, 1, 1, 0, 0)
             seconds = 3600 * 24
             indexs = (sta_ob_and_fos['time'] - time0) // np.timedelta64(1, "D")
@@ -100,7 +113,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "dayofyear":
+        elif g == "dayofyear":
             fo_times = pd.Series(0, index=sta_ob_and_fos['time'])
             if group_list_list is None:
                 grouped_dict = dict(list(sta_ob_and_fos.groupby(fo_times.index.dayofyear)))
@@ -114,7 +127,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "hour":
+        elif g == "hour":
             fo_times = pd.Series(0, index=sta_ob_and_fos['time'])
             if group_list_list is None:
                 grouped_dict = dict(list(sta_ob_and_fos.groupby(fo_times.index.hour)))
@@ -129,7 +142,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "ob_time":
+        elif g == "ob_time":
             dtimes = sta_ob_and_fos["dtime"] * np.timedelta64(1, 'h')
             obtimes = sta_ob_and_fos['time'] + dtimes
             if group_list_list is None:
@@ -145,7 +158,18 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "ob_year":
+        elif g == "ob_time_range":
+            if group_list_list is None:
+                print("当group_by = ob_time_range时 group_list_list 参数不能为None")
+            else:
+                for group_list in group_list_list:
+                    sta = meteva.base.between_ob_time_range(sta_ob_and_fos,group_list[0],group_list[1])
+                    if len(sta.index) !=0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+
+        elif g == "ob_year":
             dtimes = sta_ob_and_fos["dtime"] * np.timedelta64(1, 'h')
             obtimes = pd.Series(0, index=sta_ob_and_fos['time'] + dtimes)
             if group_list_list is None:
@@ -161,7 +185,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "ob_month":
+        elif g == "ob_month":
             dtimes = sta_ob_and_fos["dtime"] * np.timedelta64(1, 'h')
             obtimes = pd.Series(0, index=sta_ob_and_fos['time'] + dtimes)
             if group_list_list is None:
@@ -176,7 +200,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "ob_day":
+        elif g == "ob_day":
             dtimes = sta_ob_and_fos["dtime"] * np.timedelta64(1, 'h')
             obtimes = pd.Series(0, index=sta_ob_and_fos['time'] + dtimes)
             time0 = datetime.datetime(1900, 1, 1, 0, 0)
@@ -199,7 +223,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "ob_dayofyear":
+        elif g == "ob_dayofyear":
             dtimes = sta_ob_and_fos["dtime"] * np.timedelta64(1, 'h')
             obtimes = pd.Series(0, index=sta_ob_and_fos['time'] + dtimes)
             if group_list_list is None:
@@ -214,7 +238,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "ob_hour":
+        elif g == "ob_hour":
             dtimes = sta_ob_and_fos["dtime"] * np.timedelta64(1, 'h')
             obtimes = pd.Series(0, index=sta_ob_and_fos['time'] + dtimes)
             if group_list_list is None:
@@ -230,9 +254,9 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "dtime":
+        elif g == "dtime":
             if group_list_list is None:
-                grouped_dict = dict(list(sta_ob_and_fos.groupby(group_by)))
+                grouped_dict = dict(list(sta_ob_and_fos.groupby(g)))
                 keys = grouped_dict.keys()
                 for key in keys:
                     valid_group_list_list.append([key])
@@ -244,7 +268,18 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
 
-        elif group_by == "dday":
+        elif g == "dtime_range":
+            if group_list_list is None:
+                print("当group_by = dtime_range时 group_list_list 参数不能为None")
+            else:
+                for group_list in group_list_list:
+                    sta = meteva.base.between_dtime_range(sta_ob_and_fos,group_list[0],group_list[1])
+                    if len(sta.index) !=0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+
+        elif g == "dday":
             ddays = np.ceil(sta_ob_and_fos['dtime'] / 24)
             if group_list_list is None:
                 grouped_dict = dict(list(sta_ob_and_fos.groupby(ddays)))
@@ -258,7 +293,7 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "dhour":
+        elif g == "dhour":
             dhours = sta_ob_and_fos['dtime'] % 24
             if group_list_list is None:
                 grouped_dict = dict(list(sta_ob_and_fos.groupby(dhours)))
@@ -272,9 +307,9 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
-        elif group_by == "id":
+        elif g == "id":
             if group_list_list is None:
-                grouped_dict = dict(list(sta_ob_and_fos.groupby(group_by)))
+                grouped_dict = dict(list(sta_ob_and_fos.groupby(g)))
                 keys = grouped_dict.keys()
                 for key in keys:
                     valid_group_list_list.append([key])
@@ -285,6 +320,106 @@ def group(sta_ob_and_fos,group_by,group_list_list = None):
                     if len(sta.index) !=0:
                         valid_group_list_list.append(group_list)
                         sta_ob_and_fos_list.append(sta)
+
+        elif g == "lon_range":
+            if group_list_list is None:
+                print("当group_by = lon_range时 group_list_list 参数不能为None")
+            else:
+                for group_list in group_list_list:
+                    sta = meteva.base.between_lon_range(sta_ob_and_fos,group_list[0],group_list[1])
+                    if len(sta.index) !=0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+        elif g == "lat_range":
+            if group_list_list is None:
+                print("当groupy = lat_range时 group_list_list 参数不能为None")
+            else:
+                for group_list in group_list_list:
+                    sta = meteva.base.between_lat_range(sta_ob_and_fos, group_list[0], group_list[1])
+                    if len(sta.index) != 0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+        elif g == "last_range":
+            if group_list_list is None:
+                print("当group_by = last_range时 group_list_list 参数不能为None")
+            else:
+                for group_list in group_list_list:
+                    sta = meteva.base.between_last_range(sta_ob_and_fos, group_list[0], group_list[1])
+                    if len(sta.index) != 0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+        elif g == "lon_step":
+            if group_list_list is None:
+                print("当group_by = lon_range时 group_list_list 参数不能为None")
+            else:
+                group_list_list1 = []
+                start = group_list_list[0][0]
+                step = group_list_list[1][0]
+                min = np.min(sta_ob_and_fos.loc[:,"lon"])
+                max = np.max(sta_ob_and_fos.loc[:,"lon"])
+                min =start - (int((start - min) / step) + 1) *step
+                max = start + (int((max - start)/step) +1) * step
+                for value in range(min,max,step):
+                    group_list_list1.append([value,value+ step - 1e-6])
+                print(group_list_list1)
+                for group_list in group_list_list1:
+                    sta = meteva.base.between_lon_range(sta_ob_and_fos,group_list[0],group_list[1])
+                    if len(sta.index) !=0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+        elif g == "lat_step":
+            if group_list_list is None:
+                print("当group_by = lat_range时 group_list_list 参数不能为None")
+            else:
+                group_list_list1 = []
+                start = group_list_list[0][0]
+                step = group_list_list[1][0]
+                min = np.min(sta_ob_and_fos.loc[:, "lat"])
+                max = np.max(sta_ob_and_fos.loc[:, "lat"])
+                min = start - (int((start - min) / step) + 1) * step
+                max = start + (int((max - start) / step) + 1) * step
+                for value in range(min, max, step):
+                    group_list_list1.append([value, value + step - 1e-6])
+
+                for group_list in group_list_list1:
+                    sta = meteva.base.between_lat_range(sta_ob_and_fos, group_list[0], group_list[1])
+                    if len(sta.index) != 0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+
+        elif g == "last_step":
+            if group_list_list is None:
+                print("当group_by = lat_range时 group_list_list 参数不能为None")
+            else:
+                group_list_list1 = []
+                start = group_list_list[0][0]
+                step = group_list_list[1][0]
+                min = np.min(sta_ob_and_fos.iloc[:, -1])
+                max = np.max(sta_ob_and_fos.iloc[:, -1])
+
+                min = start - (int((start - min) / step) + 1) * step
+                max = start + (int((max - start) / step) + 1) * step
+                for value in range(min, max, step):
+                    group_list_list1.append([value, value + step - 1e-6])
+
+                for group_list in group_list_list1:
+                    sta = meteva.base.between_last_range(sta_ob_and_fos, group_list[0], group_list[1])
+                    if len(sta.index) != 0:
+                        valid_group_list_list.append(group_list)
+                        sta_ob_and_fos_list.append(sta)
+        elif g == "grid":
+            if group_list_list is None:
+                print("当group_by = grid时 group_list_list 参数不能为None")
+            for group_list in group_list_list:
+                sta = meteva.base.in_grid(sta_ob_and_fos, group_list[0])
+                if len(sta.index) != 0:
+                    valid_group_list_list.append(group_list)
+                    sta_ob_and_fos_list.append(sta)
+
     #返回分组结果，和实际分组方式
     if len(valid_group_list_list)==0:
         valid_group_list_list = None
