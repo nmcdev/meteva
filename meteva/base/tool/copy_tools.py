1fd97fc441dc6edce2c58df0b4ba4f9e0d007480
liucouhua <liucouhua@163.com>
2020-04-10 09:29:39 +0800

    文件格式转换支持了并行计算功能

--- meteva/base/tool/copy_tools.py
+++ meteva/base/tool/copy_tools.py
@@ -4,6 +4,89 @@ import matplotlib.colors as colors
 import time
 from meteva.base.io.GDS_data_service import GDSDataService
 from meteva.base.io import DataBlock_pb2
+from multiprocessing import freeze_support,Process,cpu_count
+
+
+
+def copy_data_one_cup(k,file_list,len_input,to_dir,input_fun,output_fun,rm_follow,ad_follow,effectiveNum,recover,grid):
+    start = time.time()
+    copyed_num = 0
+    file_num = len(file_list)
+    for i in range(file_num):
+        file = file_list[i]
+        path_input = file.replace("\\", "/")
+        path_file = path_input[len_input:]
+        path_output = to_dir + path_file
+        filename,follow = os.path.splitext(path_output)
+        if follow == rm_follow:
+            path_output = filename
+        path_output = path_output + ad_follow
+
+        if not os.path.exists(path_output) or recover:
+            meteva.base.tool.path_tools.creat_path(path_output)
+            grd = input_fun(path_input,grid = grid)
+            if grd is not None:
+                output_fun(grd, path_output, effectiveNum)
+                end = time.time()
+                copyed_num += 1
+                left_minutes = int((end - start) * (file_num - i - 1) / (copyed_num * 60)) + 1
+                print("第"+str(k)+"个进程剩余" + str(left_minutes) + "分钟")
+                # print(path_output)
+
+
+def copy_data(input_root_dir,output_root_dir,input_type,output_type,effectiveNum = 3,recover= False,grid = None,cpu_num = 1):
+    freeze_support()
+    input_root_dir = input_root_dir.replace("\\","/")
+    output_root_dir = output_root_dir.replace("\\","/")
+    if input_root_dir[-1] != "/":
+        input_root_dir += "/"
+    if output_root_dir[-1] != '/':
+        output_root_dir += "/"
+    (gds_dir,file_model) = os.path.split(input_root_dir)
+    from_dir = input_root_dir
+    to_dir = output_root_dir
+    file_list = meteva.base.tool.path_tools.get_path_list_in_dir(gds_dir)
+    file_num = len(file_list)
+    pro_num = cpu_count() - 2
+    if cpu_num > cpu_num:
+        pro_num = cpu_num
+    file_dict_list = {}
+    for i in range(pro_num):
+        file_dict_list[i] = []
+    for i in range(file_num):
+        k = i % pro_num
+        file_dict_list[k].append(file_list[i])
+
+    input_fun = None
+    rm_follow = ""
+    if input_type == "m4":
+        input_fun = meteva.base.read_griddata_from_micaps4
+    elif input_type == "nc":
+        input_fun = meteva.base.read_griddata_from_nc
+        rm_follow ="nc"
+
+    output_fun = None
+    ad_follow = ""
+    if output_type == "m4":
+        output_fun = meteva.base.write_griddata_to_micaps4
+    elif output_type == "nc":
+        output_fun = meteva.base.write_griddata_to_nc
+        ad_follow = ".nc"
+    len_input = len(input_root_dir)
+    PP = []
+    for k in range(pro_num):
+        tmpp = Process(target=copy_data_one_cup, args=(k,file_dict_list[k],len_input,to_dir,input_fun,output_fun,rm_follow,ad_follow,effectiveNum,recover,grid))
+        PP.append(tmpp)
+    print('Waiting for all subprocesses done...')
+    for pc in PP:
+        pc.start()
+
+    for pp in PP:
+        pp.join()
+    print('All subprocesses done.')
+
+
+
 
 def copy_m4_to_nc(input_root_dir,output_root_dir,effectiveNum = 3,recover= False,grid = None):
     input_root_dir = input_root_dir.replace("\\","/")
@@ -19,6 +102,7 @@ def copy_m4_to_nc(input_root_dir,output_root_dir,effectiveNum = 3,recover= False
     file_num = len(file_list)
     start = time.time()
     copyed_num = 0
+
     for i in range(file_num):
         file = file_list[i]
         path_input = file.replace("\\","/")
@@ -37,6 +121,7 @@ def copy_m4_to_nc(input_root_dir,output_root_dir,effectiveNum = 3,recover= False
                 left_minutes = int((end - start) * ( file_num - i -1) / (copyed_num * 60)) + 1
                 print("剩余" + str(left_minutes) + "分钟")
                 #print(path_output)
+
 
 
 def copy_gds_to_nc(input_root_dir,output_root_dir,effectiveNum = 3,recover= False,grid = None):
