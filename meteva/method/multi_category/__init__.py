2ea3eb383740bfe3ec5a08f4b9f291857c16d6fb
liucouhua <liucouhua@163.com>
2020-04-26 19:28:27 +0800

    update

--- meteva/method/multi_category/__init__.py
+++ meteva/method/multi_category/__init__.py
@@ -2,6 +2,6 @@
 from . import score
 from . import table
 from . import plot
-from .score import hk,hk_tcof,hss,hss_tcof,tc,tcof,accuracy,accuracy_tc,accuracy_tcof
+from .score import hk,hk_tcof,hss,hss_tcof,tc,tcof,accuracy,accuracy_tc,accuracy_tcof,hfmc_multi,ts_multi,ets_multi,bias_multi,far_multi,mr_multi
 from .table import contingency_table_multicategory,frequency_table
 from .plot import frequency_histogram
--- meteva/product/application/__init__.py
+++ meteva/product/application/__init__.py
@@ -1,3 +1,4 @@
 from .time_compare import *
-from .data_collection import download,remove,get_dati_of_path,get_dati_str_of_path
+from .data_collection import download,remove,get_dati_of_path,get_dati_str_of_path,get_hour_dhour_list
 from .read_data_base_on_gds import read_stadata,read_griddata,read_stadata_from_griddata,read_stawind,read_stawind_from_gridwind
+from .data_prepare import combine_data_from_hdf
