eeb2c41b4d6fbc183b0e85c58ee39483e2853216
liucouhua <liucouhua@163.com>
2020-06-29 17:23:04 +0800

    改进了对多模式对比的支持

--- meteva/product/program/table.py
+++ meteva/product/program/table.py
@@ -3,7 +3,7 @@ from meteva.product.program.fun import *
 from meteva.base.fun.selecting import *
 import numpy as np
 
-def table(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,para1 = None):
+def table(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,**kwargs):
     if s is not None:
         if g is not None:
             if g == "last_range" or g == "last_step":
@@ -13,7 +13,6 @@ def table(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,pa
     sta_ob_and_fos = sele_by_dict(sta_ob_and_fos0, s)
     sta_ob_and_fos_list,gll1 = group(sta_ob_and_fos,g,gll)
     data_name = meteva.base.get_stadata_names(sta_ob_and_fos)
-    fo_num = len(data_name) -1
     ensemble_score_method = [meteva.method.cr]
     group_num = len(sta_ob_and_fos_list)
 
@@ -36,18 +35,14 @@ def table(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,pa
                     valid_group_list = gll1[i]
                     valid_gll.append(gll1[i])
                 ob = sta[data_name[0]].values
-                result_all1 = []
-                for j in range(fo_num):
+                fo = sta[data_name[1:]].values.T
 
-                    fo = sta[data_name[j+1]].values
-                    save_path = get_save_path(save_dir,method,g,valid_group_list,data_name[j+1],".xlsx")
-
-                    if para1 is None:
-                        result1 = method(ob, fo,save_path = save_path)
-                    else:
-                        result1 = method(ob, fo,para1,save_path = save_path)
-                    result_all1.append(result1)
-                result_all.append(result_all1)
+                kwargs1 = copy.deepcopy(kwargs)
+                if "save_path" not in kwargs.keys():
+                    kwargs1["save_path"] = get_save_path(save_dir,method,g,valid_group_list,type = ".xlsx")
+                kwargs1["member_list"] = data_name[1:]
+                result1 = method(ob, fo, **kwargs1)
+                result_all.append(result1)
         result_all = np.array(result_all)
     result = result_all.squeeze()
 
