b4b17ce75211b54eb95c1fd6cffc598c703157e9
liucouhua <liucouhua@163.com>
2020-06-29 17:22:14 +0800

    改进了对多模式对比的支持

--- meteva/product/program/plot.py
+++ meteva/product/program/plot.py
@@ -1,7 +1,52 @@
 from meteva.base.fun import *
 from meteva.product.program.fun import *
+def plot(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,**kwargs):
+    if s is not None:
+        if g is not None:
+            if g == "last_range" or g == "last_step":
+                s["drop_last"] = False
+            else:
+                s["drop_last"] = True
+    sta_ob_and_fos = sele_by_dict(sta_ob_and_fos0, s)
+    #discription_uni = get_unique_coods(sta_ob_and_fos)
+    sta_ob_and_fos_list,gll1 = group(sta_ob_and_fos,g,gll)
+    data_name = meteva.base.get_stadata_names(sta_ob_and_fos)
+    fo_num = len(data_name) -1
+    group_num = len(sta_ob_and_fos_list)
+    title = None
+    if "title" in kwargs.keys():
+        title = kwargs["title"]
+    valid_gll = []
+    if type(method) == str:
+        method =  globals().get(method)
 
-def plot(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,title = None,para1 = None):
+    for i in range(group_num):
+        sta = sta_ob_and_fos_list[i]
+        if(len(sta.index) == 0):
+            pass
+        else:
+            valid_group_list = None
+            if gll1 is None:
+                valid_gll = None
+            else:
+                valid_group_list = gll1[i]
+                valid_gll.append(gll1[i])
+            ob = sta[data_name[0]].values
+
+            fo = sta[data_name[1:]].values.T
+            save_path = get_save_path(save_dir,method,g,valid_group_list,"",".png",title)
+            #title1 = get_title(method,g,valid_group_list,data_name[j+1],title,discription_uni)
+            title1 = meteva.product.program.get_title_from_dict(method, s, g, valid_group_list,None)
+            kwargs["title"] = title1
+            method_para = method.__code__.co_varnames[:method.__code__.co_argcount]
+            if "member_list" in method_para:
+                kwargs["member_list"] = data_name[1:]
+            method(ob, fo,save_path = save_path,**kwargs)
+
+    return valid_gll
+
+
+def plot1(sta_ob_and_fos0,method,s = None,g = None,gll = None,save_dir = None,title = None,para1 = None):
 
     if s is not None:
         if g is not None:
