64ad57e35f297dbecd93c2d6eca73edca5db0ad1
liucouhua <liucouhua@163.com>
2020-08-20 16:57:50 +0800

    新增了临近站点查询和处理的函数
    将原先位于诊断模块的查询临近固定点数的功能移入。
    增加了查询固定半径范围内的站点值的查询功能，并基于该查询结果的统计值计算功能。

---
+++ meteva/base/fun/nearing.py
@@ -0,0 +1,184 @@
+import meteva
+from meteva.base.tool.math_tools import lon_lat_to_cartesian
+from scipy.spatial import cKDTree
+import copy
+import numpy as np
+
+def sta_index_ensemble_near_by_sta(sta_to,nearNum = 100,sta_from = None,drop_frist = False):
+    if(sta_to is None):
+        return None
+    if(sta_from is None):
+        sta_from = copy.deepcopy(sta_to)
+    xyz_sta0 = lon_lat_to_cartesian(sta_to['lon'].values[:], sta_to['lat'].values[:],R = meteva.base.basicdata.ER)
+    xyz_sta1 = lon_lat_to_cartesian(sta_from['lon'].values[:], sta_from['lat'].values[:],R = meteva.base.basicdata.ER)
+    tree = cKDTree(xyz_sta1)
+    _,indexs = tree.query(xyz_sta0, k=nearNum)
+    sta_ensemble = sta_to[meteva.base.get_coord_names()]
+    for i in range(nearNum):
+        data_name = "data" + str(i)
+        sta_ensemble[data_name] = indexs[:,i]
+    if drop_frist:
+        sta_ensemble = sta_ensemble.drop(columns=['data0'])
+    return sta_ensemble
+
+def sta_id_ensemble_near_by_sta(sta_to,nearNum = 100,sta_from = None,drop_frist = False):
+    if(sta_to is None):
+        return None
+    if(sta_from is None):
+        sta_from = copy.deepcopy(sta_to)
+    xyz_sta0 = lon_lat_to_cartesian(sta_to['lon'].values[:], sta_to['lat'].values[:],R = meteva.base.basicdata.ER)
+    xyz_sta1 = lon_lat_to_cartesian(sta_from['lon'].values[:], sta_from['lat'].values[:],R = meteva.base.basicdata.ER)
+    tree = cKDTree(xyz_sta1)
+    _,indexs = tree.query(xyz_sta0, k=nearNum)
+    input_dat = sta_from.loc[:, 'id'].values
+    sta_ensemble = sta_to[meteva.base.get_coord_names()]
+    for i in range(nearNum):
+        data_name = "data" + str(i)
+        sta_ensemble[data_name] = input_dat[indexs[:,i]]
+    if drop_frist:
+        sta_ensemble = sta_ensemble.drop(columns=['data0'])
+    return sta_ensemble
+
+def sta_value_ensemble_near_by_sta(sta_to,nearNum = 100,sta_from = None,drop_frist = False):
+    if(sta_to is None):
+        return None
+    if(sta_from is None):
+        sta_from = copy.deepcopy(sta_to)
+    xyz_sta0 = lon_lat_to_cartesian(sta_to['lon'].values[:], sta_to['lat'].values[:],R = meteva.base.basicdata.ER)
+    xyz_sta1 = lon_lat_to_cartesian(sta_from['lon'].values[:], sta_from['lat'].values[:],R = meteva.base.basicdata.ER)
+    tree = cKDTree(xyz_sta1)
+    _,indexs = tree.query(xyz_sta0, k=nearNum)
+    data_name = meteva.base.get_stadata_names(sta_from)[0]
+    input_dat = sta_from[data_name].values
+    sta_ensemble = sta_to[meteva.base.get_coord_names()]
+    for i in range(nearNum):
+        data_name = "data" + str(i)
+        sta_ensemble[data_name] = input_dat[indexs[:,i]]
+    if drop_frist:
+        sta_ensemble = sta_ensemble.drop(columns=['data0'])
+    return sta_ensemble
+
+def sta_dis_ensemble_near_by_sta(sta_to,nearNum = 100,sta_from = None,drop_frist = False):
+    if(sta_to is None):
+        return None
+    if(sta_from is None):
+        sta_from = copy.deepcopy(sta_to)
+    xyz_sta0 = lon_lat_to_cartesian(sta_to['lon'].values[:], sta_to['lat'].values[:],R = meteva.base.basicdata.ER)
+    xyz_sta1 = lon_lat_to_cartesian(sta_from['lon'].values[:], sta_from['lat'].values[:],R = meteva.base.basicdata.ER)
+    tree = cKDTree(xyz_sta1)
+    d,_ = tree.query(xyz_sta0, k=nearNum)
+    sta_ensemble = sta_to[meteva.base.get_coord_names()]
+    for i in range(nearNum):
+        data_name = "data" + str(i)
+        sta_ensemble[data_name] = d[:,i]
+    if drop_frist:
+        sta_ensemble = sta_ensemble.drop(columns=['data0'])
+    return sta_ensemble
+
+def sta_index_ensemble_near_by_grid(sta, grid,nearNum = 1):
+    ER = meteva.base.ER
+    members = np.arange(nearNum).tolist()
+    grid1 = meteva.base.grid(grid.glon,grid.glat,member_list=members)
+    grd_en = meteva.base.grid_data(grid1)
+    xyz_sta =  meteva.base.tool.math_tools.lon_lat_to_cartesian(sta.loc[:,"lon"], sta.loc[:,"lat"],R = ER)
+    lon = np.arange(grid1.nlon) * grid1.dlon + grid1.slon
+    lat = np.arange(grid1.nlat) * grid1.dlat + grid1.slat
+    grid_lon,grid_lat = np.meshgrid(lon,lat)
+    xyz_grid = meteva.base.tool.math_tools.lon_lat_to_cartesian(grid_lon.flatten(), grid_lat.flatten(),R = ER)
+    tree = cKDTree(xyz_sta)
+    value, inds = tree.query(xyz_grid, k=nearNum)
+    grd_en.values = inds.reshape((nearNum,1,1,1,grid1.nlat,grid1.nlon))
+    return grd_en
+
+def values_list_list_in_r_of_sta(sta_to, r = 40, sta_from = None,drop_first = False):
+    '''
+
+    :param sta_to:
+    :param r:
+    :param sta_from:
+    :param drop_first:
+    :return: 返回的站点将会和sta_to 一致
+    '''
+    if(sta_to is None):
+        return None
+    if(sta_from is None):
+        sta_from = copy.deepcopy(sta_to)
+    sta_from = meteva.base.not_IV(sta_from)
+    #print(sta_from)
+    xyz_sta0 = lon_lat_to_cartesian(sta_to['lon'].values[:], sta_to['lat'].values[:],R = meteva.base.basicdata.ER)
+    xyz_sta1 = lon_lat_to_cartesian(sta_from['lon'].values[:], sta_from['lat'].values[:],R = meteva.base.basicdata.ER)
+    tree = cKDTree(xyz_sta1)
+    data_name = meteva.base.get_stadata_names(sta_from)[0]
+    input_dat = sta_from[data_name].values
+    indexs_list = tree.query_ball_point(xyz_sta0,r=r)
+
+    values_list = []
+    if drop_first:
+        for indexs in indexs_list:
+            if len(indexs) >1:
+                values = input_dat[indexs[1:]]
+                values_list.append(values)
+            else:
+                values_list.append([meteva.base.IV])
+    else:
+        for indexs in indexs_list:
+            values = input_dat[indexs]
+            values_list.append(values)
+
+    return values_list
+
+def statistic_in_r_of_sta(sta_to,operation, r = 40, sta_from = None,drop_first = False):
+    '''
+    :param sta_to:
+    :param r:
+    :param sta_from:
+    :param drop_first:
+    :return: 返回的站点将会和sta_to 一致
+    '''
+    sta_result = sta_to[meteva.base.get_coord_names()]
+    values_list_list = values_list_list_in_r_of_sta(sta_to, r = r, sta_from = sta_from,drop_first = drop_first)
+    statistic_value = []
+    #print(values_list_list)
+    nsta = len(values_list_list)
+    for n in  range(nsta):
+        values_list = values_list_list[n]
+        if(len(values_list)==0):
+            statistic_value.append(meteva.base.IV)
+        else:
+            statistic_value.append(operation(values_list))
+    statistic_value = np.array(statistic_value)
+    sta_result[operation.__name__] = statistic_value
+    return sta_result
+
+def max_in_r_of_sta(sta_to, r = 40, sta_from = None,drop_first = False):
+    '''
+    :param sta_to:
+    :param r:
+    :param sta_from:
+    :param drop_first:
+    :return: 返回的站点将会和sta_to 一致
+    '''
+
+    return statistic_in_r_of_sta(sta_to,np.max, r = r, sta_from = sta_from,drop_first = drop_first)
+
+def min_in_r_of_sta(sta_to, r = 40, sta_from = None,drop_first = False):
+    '''
+    :param sta_to:
+    :param r:
+    :param sta_from:
+    :param drop_first:
+    :return: 返回的站点将会和sta_to 一致
+    '''
+
+    return statistic_in_r_of_sta(sta_to,np.min, r = r, sta_from = sta_from,drop_first = drop_first)
+
+def mean_in_r_of_sta(sta_to, r = 40, sta_from = None,drop_first = False):
+    '''
+    :param sta_to:
+    :param r:
+    :param sta_from:
+    :param drop_first:
+    :return: 返回的站点将会和sta_to 一致
+    '''
+
+    return statistic_in_r_of_sta(sta_to,np.mean, r = r, sta_from = sta_from,drop_first = drop_first)
