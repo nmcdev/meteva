e5cdeeca33b1bd070aaf8d976b3838b2c38cfea6
liucouhua <liucouhua@163.com>
2020-09-04 09:46:05 +0800

    为连续型变量新增检验指标增加索引

--- meteva/method/continuous/__init__.py
+++ meteva/method/continuous/__init__.py
@@ -6,7 +6,9 @@ from . import skill
 
 from .table import *
 from .plot import scatter_regress,pdf_plot,box_plot_continue
-from .skill import mre_skill
+from .skill import mre_skill,sme,sst
 from .score import sample_count,ob_mean,fo_mean,tase,me,mae,mse,rmse,me_tase,mae_tase,mse_tase,rmse_tase,tc_count
 from .score import bias_m,bias_tmmsss,corr,corr_tmmsss,tmmsss,tmmsss_merge,toar,nse,nse_tase_tmmsss,mre,mre_toar,correct_rate,correct_rate_tc
 from .score import ob_fo_mean,ob_fo_std,ob_fo_max,ob_fo_min
+from .score import wrong_rate,wrong_rate_tc,residual_error,residual_error_tmmsss,residual_error_rate,residual_error_rate_tmmsss
+from .score import max_abs_error,max_error,min_error
