c1007d3d9b365ea1643917be1f6a458d41085d75
liucouhua <liucouhua@163.com>
2020-09-04 09:57:38 +0800

    更新了版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 from codecs import open
 name = "meteva"
 author ="liucouhua,daikan,wangbaoli"
-version ="1.3.3.2"
+version ="1.3.6"
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
