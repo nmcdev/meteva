2ce94bbf7d77bcb08afb2afe946aa65b809b3f44
liucouhua <liucouhua@163.com>
2020-11-01 16:54:30 +0800

    增加了performance_grade 模块

--- meteva/method/multi_category/__init__.py
+++ meteva/method/multi_category/__init__.py
@@ -5,4 +5,4 @@ from . import plot
 from .score import hk,hk_tcof,hss,hss_tcof,tc,tcof,accuracy,accuracy_tc,accuracy_tcof,hfmc_multi,ts_multi,ets_multi,bias_multi,far_multi,mr_multi
 from .score import hfmc_grade,ts_grade,ets_grade,bias_grade,far_grade,mr_grade
 from .table import contingency_table_multicategory,frequency_table
-from .plot import frequency_histogram
+from .plot import frequency_histogram,performance_grade
