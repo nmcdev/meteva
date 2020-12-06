48eadbb2790909328681a262135194aac6f0aa17
liucouhua <liucouhua@163.com>
2020-12-06 21:19:33 +0800

    改进了标题

--- meteva/product/program/sample_statistic.py
+++ meteva/product/program/sample_statistic.py
@@ -3,13 +3,13 @@ import meteva
 
 def sample_tdt(sta_all,return_result = False,**kwargs):
     result = meteva.product.score_tdt(sta_all,meteva.method.sample_count,x_y="time_dtime",annot = -1,
-                                      title = "样本数随时间和时效的分布",**kwargs)
+                                      title = ["样本数随时间和时效的分布"],**kwargs)
     if return_result:
         return result
 
 def sample_id(sta_all,return_result = False,**kwargs):
     result = meteva.product.score_id(sta_all,meteva.method.sample_count,plot = "scatter",
-                                     title = "所有时间和时效样本数的空间分布",
+                                     title = ["所有时间和时效样本数的空间分布"],
                                     **kwargs)
     if return_result:
         return result
