10e87eef1e8ecf6ef32818f4814178ee5e9a415b
liucouhua <liucouhua@163.com>
2021-01-18 11:11:10 +0800

    增加了多分类预报检验综合图
    并修复了分级检验综合图中的bug

--- meteva/method/multi_category/__init__.py
+++ meteva/method/multi_category/__init__.py
@@ -4,5 +4,6 @@ from . import table
 from . import plot
 from .score import hk,hk_tcof,hss,hss_tcof,tc,tcof,accuracy,accuracy_tc,accuracy_tcof,hfmc_multi,ts_multi,ets_multi,bias_multi,far_multi,mr_multi
 from .score import hfmc_grade,ts_grade,ets_grade,bias_grade,far_grade,mr_grade
+from .score import pod_grade,pofd_grade,sr_grade,pod_multi,pofd_multi,sr_multi
 from .table import contingency_table_multicategory,frequency_table
-from .plot import frequency_histogram,performance_grade
+from .plot import frequency_histogram,performance_grade,performance_multi
