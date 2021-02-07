c212b6e5b320474493b54b6bd8059752d2da6149
liucouhua <liucouhua@163.com>
2021-02-07 11:39:35 +0800

    完善了风矢量检验图形产品的子图布局

--- meteva/method/Vector/plot.py
+++ meteva/method/Vector/plot.py
@@ -35,45 +35,54 @@ def scatter_uv_error(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量�
     new_Fo_shape = new_u_Fo.shape
     sub_plot_num = new_Fo_shape[0]
 
+
+
+    height_title = sup_fontsize * 0.01
+    height_bottem_xticsk = sup_fontsize * 0.05
+    height_hspace = sup_fontsize * 0.07
+
+    width_wspace = height_hspace
+
+    width_colorbar = 0.5
+    width_left_yticks = sup_fontsize * 0.1
+
+
     if ncol is None:
         if sub_plot_num ==1:
-            ncols = 1
+            ncol = 1
         elif sub_plot_num %2 == 0:
-            ncols = 2
+            ncol = 2
         else:
-            ncols = 3
+            ncol = 3
+
+    if title is None:
+        sup_height_title = 0
     else:
-        ncols = ncol
-
-    nrows = math.ceil(new_Fo_shape[0] / ncols)
-
-    if height is None:
-        if nrows==1:
-            if ncols <3:
-                height_fig = 3.5
-            else:
-                height_fig = 2.5
+        sup_height_title = sup_fontsize * 0.12
+    nrow = math.ceil(new_Fo_shape[0] / ncol)
+    if width is None and height is None:
+        if sub_plot_num ==1:
+            width = 5
         else:
-            if ncols > nrows:
-                height_fig = 6
-            else:
-                height_fig = 7
-    else:
-        height_fig = height
+            width = 10
 
-    height_suptitle = 0.1
-    height_xticks_title = 0.1
-    height_hspace = 0.6
-    heidht_axis = (height_fig - height_suptitle - height_xticks_title - height_hspace * (nrows - 1)) / nrows
-    width_axis = heidht_axis
-    width_yticks = 0.15
-    width_wspace = width_yticks * 5
+
     if width is None:
-        width_fig = width_axis * ncols + width_wspace * (ncols - 1) + width_yticks
+        height_all_plot = height - height_title - height_bottem_xticsk - (nrow-1) * height_hspace + sup_height_title
+        height_axis = height_all_plot / nrow
+        width_axis = height_axis
+        width_all_plot = width_axis * ncol + (ncol-1) * width_wspace
+        width = width_all_plot + width_colorbar + width_left_yticks
     else:
-        width_fig = width
+        width_all_plot = width - width_colorbar - width_left_yticks - (ncol - 1) * width_wspace
+        width_axis = width_all_plot / ncol
+        height_axis = width_axis
+        height_all_plot = height_axis * nrow + (nrow-1) * height_hspace
+        height = height_all_plot + height_title + height_bottem_xticsk + sup_height_title
 
-    fig = plt.figure(figsize=(width_fig,height_fig),dpi = dpi)
+
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
+    plt.subplots_adjust(hspace = 0.3,wspace = 0.3)
     u1 = u_ob.flatten()
     v1 = v_ob.flatten()
 
@@ -85,14 +94,25 @@ def scatter_uv_error(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量�
     colors = meteva.base.color_tools.get_color_list(new_Fo_shape[0]+1)
 
     for line in range(new_Fo_shape[0]):
+        pi = line % ncol
+        pj = int(line / ncol)
+        rect1 = [(width_left_yticks + pi * (width_axis + width_wspace))/width,
+                 (height_bottem_xticsk + (nrow -1- pj) * (height_axis + height_hspace))/height,
+                 width_axis / width,
+                 height_axis / height]
+        ax = plt.axes(rect1)
+
         u2 = new_u_Fo[line, :].flatten()
         v2 = new_v_Fo[line, :].flatten()
-        markersize = 5 * width_axis * heidht_axis / np.sqrt(u_ob.size)
+
+
+        markersize = 5 * width_axis * height_axis / np.sqrt(u_ob.size)
+
         if markersize < 1:
             markersize = 1
         elif markersize > 20:
             markersize = 20
-        plt.subplot(nrows, ncols, line + 1)
+        #plt.subplot(nrows, ncols, line + 1)
 
 
         plt.plot(u2-u1,v2-v1,'.',color = colors[line+1], markersize=markersize)
@@ -121,8 +141,10 @@ def scatter_uv_error(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量�
             y = r * np.cos(angle)
             plt.plot(x,y,"--",color = "k",linewidth = 0.5)
 
-    titlelines = title.split("\n")
-    fig.suptitle(title, fontsize=sup_fontsize, y=0.99+0.01 * len(titlelines))
+    y_sup_title = (height_bottem_xticsk + (nrow) * (height_axis + height_hspace)) / height
+    if title is not None:
+        plt.suptitle(title, y = y_sup_title,fontsize=sup_fontsize * 1.2)
+
     if(save_path is not None):
         file1,extension = os.path.splitext(save_path)
         extension = extension[1:]
@@ -132,6 +154,7 @@ def scatter_uv_error(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量�
     if show:
         plt.show()
     plt.close()
+
 
 def scatter_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量散点分布图"
                , vmax=None, ncol=None, save_path=None, show=False, dpi=300,
@@ -160,64 +183,80 @@ def scatter_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量散点�
     new_Fo_shape = new_u_Fo.shape
     sub_plot_num = new_Fo_shape[0]
 
+    height_title = sup_fontsize * 0.01
+    height_bottem_xticsk = sup_fontsize * 0.05
+    height_hspace = sup_fontsize * 0.07
+
+    width_wspace = height_hspace
+
+    width_colorbar = 0.5
+    width_left_yticks = sup_fontsize * 0.1
+
+
     if ncol is None:
         if sub_plot_num ==1:
-            ncols = 1
+            ncol = 1
         elif sub_plot_num %2 == 0:
-            ncols = 2
+            ncol = 2
         else:
-            ncols = 3
+            ncol = 3
+
+
+    if title is None:
+        sup_height_title = 0
     else:
-        ncols = ncol
-
-    nrows = math.ceil(new_Fo_shape[0] / ncols)
-
-    if height is None:
-        if nrows==1:
-            if ncols <3:
-                height_fig = 3.5
-            else:
-                height_fig = 2.5
+        sup_height_title = sup_fontsize * 0.12
+    nrow = math.ceil(new_Fo_shape[0] / ncol)
+    if width is None and height is None:
+        if sub_plot_num ==1:
+            width = 5
         else:
-            if ncols > nrows:
-                height_fig = 6
-            else:
-                height_fig = 7
-    else:
-        height_fig = height
+            width = 10
 
-    height_suptitle = 0.1
-    height_xticks_title = 0.1
-    height_hspace = 0.6
-    heidht_axis = (height_fig - height_suptitle - height_xticks_title - height_hspace * (nrows - 1)) / nrows
-    width_axis = heidht_axis
-    width_yticks = 0.15
-    width_wspace = width_yticks * 5
+
     if width is None:
-        width_fig = width_axis * ncols + width_wspace * (ncols - 1) + width_yticks
+        height_all_plot = height - height_title - height_bottem_xticsk - (nrow-1) * height_hspace + sup_height_title
+        height_axis = height_all_plot / nrow
+        width_axis = height_axis
+        width_all_plot = width_axis * ncol + (ncol-1) * width_wspace
+        width = width_all_plot + width_colorbar + width_left_yticks
     else:
-        width_fig = width
+        width_all_plot = width - width_colorbar - width_left_yticks - (ncol - 1) * width_wspace
+        width_axis = width_all_plot / ncol
+        height_axis = width_axis
+        height_all_plot = height_axis * nrow + (nrow-1) * height_hspace
+        height = height_all_plot + height_title + height_bottem_xticsk + sup_height_title
 
-    fig = plt.figure(figsize=(width_fig,height_fig),dpi = dpi)
+
     u1 = u_ob.flatten() + np.random.randn(len(u_ob))*add_randn_to_ob
     v1 = v_ob.flatten() + np.random.randn(len(v_ob))*add_randn_to_ob
-
 
     if member_list is None:
         member_list = []
         for line in range(new_Fo_shape[0]):
             member_list.append("预报" + str(line))
 
+
+
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
     colors = meteva.base.color_tools.get_color_list(new_Fo_shape[0]+1)
+
     for line in range(new_Fo_shape[0]):
+        pi = line % ncol
+        pj = int(line / ncol)
+        rect1 = [(width_left_yticks + pi * (width_axis + width_wspace))/width,
+                 (height_bottem_xticsk + (nrow -1- pj) * (height_axis + height_hspace))/height,
+                 width_axis / width,
+                 height_axis / height]
+        ax = plt.axes(rect1)
         u2 = new_u_Fo[line, :].flatten()
         v2 = new_v_Fo[line, :].flatten()
-        markersize = 15 * width_axis * heidht_axis / np.sqrt(u_ob.size)
+        markersize = 15 * width_axis * height_axis / np.sqrt(u_ob.size)
         if markersize < 1:
             markersize = 1
         elif markersize > 20:
             markersize = 20
-        plt.subplot(nrows, ncols, line + 1)
+        #plt.subplot(nrow, ncols, line + 1)
         plt.plot(u1,v1,'.',color= "r", markeredgewidth = 0, markersize=markersize,alpha = 0.5,label = "OBS")
         plt.plot(u2,v2,'.',color= "b", markeredgewidth = 0,  markersize=markersize,alpha = 0.5,label = "FCT")
 
@@ -225,10 +264,10 @@ def scatter_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量散点�
         plt.ylabel("V分量",fontsize = sup_fontsize *0.9)
         plt.title(member_list[line],fontsize = sup_fontsize)
 
-        #print(maxs)
+
         plt.xlim(-vmax,vmax)
         plt.ylim(-vmax,vmax)
-        plt.legend()
+        plt.legend(loc = 2,fontsize = sup_fontsize * 0.7)
         angles = np.arange(0,360,45)
         for i in range(len(angles)):
             angle = angles[i] * 3.1415926 /180
@@ -244,9 +283,13 @@ def scatter_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量散点�
             x = r * np.sin(angle)
             y = r * np.cos(angle)
             plt.plot(x,y,"--",color = "k",linewidth = 0.5)
+        plt.xticks(fontsize = 0.8 * sup_fontsize)
+        plt.yticks(fontsize = 0.8 * sup_fontsize)
 
-    titlelines = title.split("\n")
-    fig.suptitle(title, fontsize=sup_fontsize, y=0.99+0.01 * len(titlelines))
+    y_sup_title = (height_bottem_xticsk + (nrow) * (height_axis + height_hspace)) / height
+    if title is not None:
+        plt.suptitle(title, y = y_sup_title,fontsize=sup_fontsize * 1.2)
+
     if(save_path is not None):
         file1,extension = os.path.splitext(save_path)
         extension = extension[1:]
@@ -340,45 +383,53 @@ def statisitic_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量分�
     new_Fo_shape = new_u_Fo.shape
     sub_plot_num = new_Fo_shape[0]
 
+
+    height_title = sup_fontsize * 0.01
+    height_bottem_xticsk = sup_fontsize * 0.05
+    height_hspace = sup_fontsize * 0.07
+
+    width_wspace = height_hspace
+
+    width_colorbar = 1
+    width_left_yticks = sup_fontsize * 0.1
+
+
     if ncol is None:
         if sub_plot_num ==1:
-            ncols = 1
+            ncol = 1
         elif sub_plot_num %2 == 0:
-            ncols = 2
+            ncol = 2
         else:
-            ncols = 3
+            ncol = 3
+
+    if title is None:
+        sup_height_title = 0
     else:
-        ncols = ncol
-
-    nrows = math.ceil(new_Fo_shape[0] / ncols)
-
-    if height is None:
-        if nrows==1:
-            if ncols <3:
-                height_fig = 3.5
-            else:
-                height_fig = 2.5
+        sup_height_title = sup_fontsize * 0.12
+    nrow = math.ceil(new_Fo_shape[0] / ncol)
+    if width is None and height is None:
+        if sub_plot_num ==1:
+            width = 5
         else:
-            if ncols > nrows:
-                height_fig = 6
-            else:
-                height_fig = 7
-    else:
-        height_fig = height
+            width = 10
 
-    height_suptitle = 0.1
-    height_xticks_title = 0.1
-    height_hspace = 0.6
-    heidht_axis = (height_fig - height_suptitle - height_xticks_title - height_hspace * (nrows - 1)) / nrows
-    width_axis = heidht_axis
-    width_yticks = 0.15
-    width_wspace = width_yticks * 5
+
     if width is None:
-        width_fig = width_axis * ncols + width_wspace * (ncols - 1) + width_yticks
+        height_all_plot = height - height_title - height_bottem_xticsk - (nrow-1) * height_hspace + sup_height_title
+        height_axis = height_all_plot / nrow
+        width_axis = height_axis
+        width_all_plot = width_axis * ncol + (ncol-1) * width_wspace
+        width = width_all_plot + width_colorbar + width_left_yticks
     else:
-        width_fig = width
+        width_all_plot = width - width_colorbar - width_left_yticks - (ncol - 1) * width_wspace
+        width_axis = width_all_plot / ncol
+        height_axis = width_axis
+        height_all_plot = height_axis * nrow + (nrow-1) * height_hspace
+        height = height_all_plot + height_title + height_bottem_xticsk + sup_height_title
 
-    fig = plt.figure(figsize=(width_fig,height_fig),dpi = dpi)
+
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
+    #plt.subplots_adjust(hspace = 0.3,wspace = 0.30)
     u1 = u_ob.flatten()
     v1 = v_ob.flatten()
 
@@ -421,7 +472,15 @@ def statisitic_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量分�
     ax_ob = None
     ax_fo = None
     for line in range(new_Fo_shape[0]):
-        plt.subplot(nrows, ncols, line + 1)
+
+        pi = line % ncol
+        pj = int(line / ncol)
+        rect1 = [(width_left_yticks + pi * (width_axis + width_wspace))/width,
+                 (height_bottem_xticsk + (nrow -1- pj) * (height_axis + height_hspace))/height,
+                 width_axis / width,
+                 height_axis / height]
+        ax = plt.axes(rect1)
+
         ax_ob = plt.scatter(mu1, mv1, c=gray1,s = mf1,cmap = cmap1,norm=norm1)
         ax_fo = plt.scatter(mu2_list[line], mv2_list[line], c=mgray2_list[line],s = mf2_list[line],cmap = cmap2,norm = norm2)
 
@@ -449,17 +508,29 @@ def statisitic_uv(u_ob,u_fo,v_ob,v_fo,member_list = None,title = "风矢量分�
             y = r * np.cos(angle)
             plt.plot(x,y,"--",color = "k",linewidth = 0.5)
 
-    colorbar_position_grid = fig.add_axes([0.92, 0.50, 0.02, 0.32])  # 位置[左,下,宽,高]
+
+    left_low = (width_left_yticks + ncol * width_axis  + (ncol-1)*width_wspace+0.1)/width
+    #print(left_low)
+    colorbar_position_grid = fig.add_axes([left_low, height_bottem_xticsk / height,0.02, 0.455*height_all_plot/height])  # 位置[左,下,宽,高]
+
+
+
+    #colorbar_position_grid = fig.add_axes([0.92, 0.50, 0.02, 0.32])  # 位置[左,下,宽,高]
     colorbar_ob = plt.colorbar(ax_ob, cax=colorbar_position_grid)
     colorbar_ob.ax.tick_params(labelsize=sup_fontsize * 0.5)  # 改变bar标签字体大小
 
     colorbar_ob.set_label('观测风速的一致性',fontsize = sup_fontsize * 0.7)
-    colorbar_position_grid = fig.add_axes([0.92, 0.15, 0.02, 0.32])  # 位置[左,下,宽,高]
+    #colorbar_position_grid = fig.add_axes([0.92, 0.15, 0.02, 0.32])  # 位置[左,下,宽,高]
+    colorbar_position_grid = fig.add_axes([left_low, (height_bottem_xticsk+0.545 * height_all_plot) / height,
+                                           0.02, 0.455*height_all_plot/height])  # 位置[左,下,宽,高]
+
+
     colorbar_fo = plt.colorbar(ax_fo, cax=colorbar_position_grid)
     colorbar_fo.ax.tick_params(labelsize=sup_fontsize * 0.5)  # 改变bar标签字体大小
     colorbar_fo.set_label('预报风速的一致性',fontsize = sup_fontsize * 0.7)
-    titlelines = title.split("\n")
-    fig.suptitle(title, fontsize=sup_fontsize, y=0.99+0.01 * len(titlelines))
+    y_sup_title = (height_bottem_xticsk + (nrow) * (height_axis + height_hspace)) / height
+    if title is not None:
+        plt.suptitle(title, y = y_sup_title,fontsize=sup_fontsize * 1.2)
     if(save_path is not None):
         file1,extension = os.path.splitext(save_path)
         extension = extension[1:]
