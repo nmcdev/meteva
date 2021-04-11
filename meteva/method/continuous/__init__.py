d121f4528ee590833f06e4487e27c5b9cfd7a5f9
liucouhua <liucouhua@163.com>
2021-04-11 08:23:32 +0800

    增加了绘制误差频率图的模块

--- meteva/method/continuous/__init__.py
+++ meteva/method/continuous/__init__.py
@@ -5,7 +5,7 @@ from . import plot
 from . import skill
 
 from .table import *
-from .plot import scatter_regress,pdf_plot,box_plot_continue,taylor_diagram
+from .plot import scatter_regress,pdf_plot,box_plot_continue,taylor_diagram,frequency_histogram_error
 from .skill import mre_skill,sme,sst
 from .score import sample_count,ob_mean,fo_mean,tase,me,mae,mse,rmse,me_tase,mae_tase,mse_tase,rmse_tase,tc_count
 from .score import bias_m,bias_tmmsss,corr,corr_tmmsss,tmmsss,tmmsss_merge,toar,nse,nse_tase_tmmsss,mre,mre_toar,correct_rate,correct_rate_tc
