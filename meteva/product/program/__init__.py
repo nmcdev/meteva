cdf10ed65183cae0ae792032d59cff3d355d4bc3
liucouhua <liucouhua@163.com>
2021-04-11 08:26:22 +0800

    Update __init__.py

--- meteva/product/program/__init__.py
+++ meteva/product/program/__init__.py
@@ -1,5 +1,6 @@
 from .time_compare import time_list_mesh_error,time_list_mesh_temp,time_list_mesh_tcdc,time_list_mesh_vis,time_list_mesh_rain01h,time_list_mesh_rain03h,time_list_mesh_wind,time_list_mesh,time_list_mesh_rh,time_list_line,time_list_line_error
 from .space_compare import rain_24h_sg,rain_24h_comprehensive_sg,rain_24h_comprehensive_chinaland_sg,temper_comprehensive_gg,temper_gg
+from .space_compare import rain_sg,rain_comprehensive_sg,rain_comprehensive_chinaland_sg
 from .score import score,score_id,score_tdt
 from .table import table
 from .plot import plot
