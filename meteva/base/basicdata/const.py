290f8704aa4cfdb2e7aa22685f106dd588415b0e
liucouhua <liucouhua@163.com>
2021-05-13 23:04:33 +0800

    增加温度检验高度订正功能
    在基于站点观测对网格温度进行检验时，有时站点在山顶，周围格点在山脚，或者站点在山谷，周围网格在腰或山顶，此时采用双线性插值将网格温度插值到站点，会存在一个系统性的偏差。在温度检验时理应扣除该偏差。该偏差的幅度除了和高度有关，也和温度垂直递减率有关，然而目前尚没有合理的方式动态的估算温度垂直递减率，因此本算法仅仅考虑温度垂直递减率为常数0.6℃/100m的情况。

--- meteva/base/basicdata/const.py
+++ meteva/base/basicdata/const.py
@@ -13,6 +13,7 @@ station_国家站 = pkg_resources.resource_filename('meteva', "resources/station
 station_国家站_考核区域站 = pkg_resources.resource_filename('meteva', "resources/stations/stat10461.txt")
 station_全球城市 = pkg_resources.resource_filename('meteva', "resources/stations/station_global_alt_11621.txt")
 station_全球重点城市 = pkg_resources.resource_filename('meteva', "resources/stations/sta_global_alt_243.txt")
+terrain_height_correction_data = pkg_resources.resource_filename('meteva', "resources/stations/geography_temp_delta.txt")
 gds_ip_port = None
 customized_basemap_list = None
 beaufort_scale = [0.0,0.3,1.6,3.4,5.5,8.0,10.8,13.9,17.2,20.8,24.5,28.5,32.7,37,41.5,46.2,51,56.1]
