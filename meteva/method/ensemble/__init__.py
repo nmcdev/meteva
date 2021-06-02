9d880879873f59bee28aa18eab1827d38a8306c0
liucouhua <liucouhua@163.com>
2021-06-02 15:02:56 +0800

    增加了crps指标

--- meteva/method/ensemble/__init__.py
+++ meteva/method/ensemble/__init__.py
@@ -2,6 +2,6 @@
 from . import score
 from . import table
 from . import plot
-from .score import cr
+from .score import cr,crps
 from .table import *
 from .plot import box_plot_ensemble,rank_histogram
