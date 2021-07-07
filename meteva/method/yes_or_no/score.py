47b076d2d532b695e47cac2dfc8d26caf8c0033e
liucouhua <liucouhua@163.com>
2021-07-07 15:07:33 +0800

    改进了f_score计算函数

--- meteva/method/yes_or_no/score.py
+++ meteva/method/yes_or_no/score.py
@@ -768,7 +768,7 @@ def orss(Ob,Fo,grade_list= [1e-30],compair = ">="):
 
 def fscore(Ob,Fo,grade_list = [1e-30],compair = ">=",belta = 1):
     '''
-    = (1+belata^2)*(pod*sr)/(belta^2 * sr + pod)
+    = (1+belta^2)*(pod*sr)/(belta^2 * sr + pod)
     :param Ob:
     :param Fo:
     :param grade_list:
@@ -776,22 +776,36 @@ def fscore(Ob,Fo,grade_list = [1e-30],compair = ">=",belta = 1):
     :param belta:
     :return:
     '''
-    precision = sr(Ob,Fo,grade_list=grade_list,compair=compair)
-    recall = pod(Ob,Fo,grade_list=grade_list,compair=compair)
+    #precision = sr(Ob,Fo,grade_list=grade_list,compair=compair)
+    #recall = pod(Ob,Fo,grade_list=grade_list,compair=compair)
 
-    f_score = (1 + belta * belta) * (precision * recall)/(belta * belta * precision + recall)
+    #f_score = (1 + belta * belta) * (precision * recall)/(belta * belta * precision + recall)
+    hfmc_array= hfmc(Ob,Fo,grade_list,compair)
+    f_score = fscore_hfmc(hfmc_array,belta)
 
     return f_score
 
 def fscore_hfmc(hfmc_array,belta = 1):
-    precision = sr_hfmc(hfmc_array)
-    recall = pod_hfmc(hfmc_array)
+    #precision = sr_hfmc(hfmc_array)
+    #recall = pod_hfmc(hfmc_array)
 
-    f_score = (1 + belta * belta) * (precision * recall) / (belta * belta * precision + recall)
-    if f_score.size ==1:
-        f_score = f_score[0]
+    #f_score = (1 + belta * belta) * (precision * recall) / (belta * belta * precision + recall)
+    #if f_score.size ==1:
+    #    f_score = f_score[0]
 
-    return f_score
+
+    hit = hfmc_array[...,0]
+    fal = hfmc_array[...,1]
+    mis = hfmc_array[...,2]
+
+    sum = (1+belta*belta)*hit + belta * belta* mis + fal
+    sum[sum == 0] = -1
+    fscore_array =(1+belta*belta)*hit/ sum
+    fscore_array[sum == -1] = IV
+    if fscore_array.size ==1:
+        fscore_array = fscore_array[0]
+
+    return fscore_array
 
 def dts(Ob,Fo,grade_list= [1e-30],compair = ">="):
     hfmc_array = hfmc(Ob, Fo, grade_list,compair = compair)
