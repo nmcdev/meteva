7a6334b5adf6833340cd4a31b52cfc0e0bb7a583
liucouhua <liucouhua@163.com>
2021-11-02 23:12:59 +0800

    改进了样本时间时效分别统计函数

--- meteva/product/program/sample_statistic.py
+++ meteva/product/program/sample_statistic.py
@@ -6,7 +6,7 @@ def sample_tdt(sta_all,return_result = False,**kwargs):
         kwargs["title"] =  ["样本数随时间和时效的分布"]
     if "x_y" not in kwargs.keys():
         kwargs["x_y"] ="time_dtime"
-    result = meteva.product.score_tdt(sta_all,meteva.method.sample_count,annot = -1
+    result = meteva.product.score_tdt(sta_all,meteva.method.sample_count,annot = -1,add_min_xticks = True
                                       ,**kwargs)
     if return_result:
         return result
