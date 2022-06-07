6d1c936b9494a8dfb9f597ad32b92cddf78a020e
liucouhua <liucouhua@163.com>
2022-06-07 16:18:43 +0800

    优化了统计样本的模块

--- meteva/product/program/sample_statistic.py
+++ meteva/product/program/sample_statistic.py
@@ -6,7 +6,10 @@ def sample_tdt(sta_all,return_result = False,**kwargs):
         kwargs["title"] =  ["样本数随时间和时效的分布"]
     if "x_y" not in kwargs.keys():
         kwargs["x_y"] ="time_dtime"
-    result = meteva.product.score_tdt(sta_all,meteva.method.sample_count,annot = -1,add_min_xticks = True
+    kwargs["width"] = 10
+    sta_all_0 = sta_all.copy()
+    sta_all_0.iloc[:,6:] = 0
+    result = meteva.product.score_tdt(sta_all_0,meteva.method.sample_count,annot = -1,add_min_xticks = True
                                       ,**kwargs)
     if return_result:
         return result
@@ -14,8 +17,9 @@ def sample_tdt(sta_all,return_result = False,**kwargs):
 def sample_id(sta_all,return_result = False,**kwargs):
     if "title" not in kwargs.keys():
         kwargs["title"] =  ["所有时间和时效样本数的空间分布"]
-    result = meteva.product.score_id(sta_all,meteva.method.sample_count,plot = "scatter",
-
+    sta_all_0 = sta_all.copy()
+    sta_all_0.iloc[:,6:] = 0
+    result = meteva.product.score_id(sta_all_0,meteva.method.sample_count,plot = "scatter",
                                     **kwargs)
     if return_result:
         return result
