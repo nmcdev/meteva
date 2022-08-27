20067a64e5e162c6cb1e5dc26733caacf99b051c
liucouhua <liucouhua@163.com>
2022-08-27 16:11:06 +0800

    增加了判断某个时刻是否为日最大（小）值的函数

--- meteva/base/fun/timing.py
+++ meteva/base/fun/timing.py
@@ -176,3 +176,82 @@ def rain_process_peak(rain01h_ob_fos,used_coords = "time"):
     peak_value = meteva.base.mutiply_on_level_time_dtime_id(rain01h_ob_fos,peak_time)
 
     return peak_time,peak_value
+
+
+def is_max_of_oneday(sta,used_coords = "time",start_hour_of_one_day = 0,keep_zeros = True):
+    '''
+    判断一个数据样本是否为一天中的最大值，是则记为1，否则记为0
+    :param sta:
+    :param used_coords:
+    :param start_hour_of_one_day:
+    :return:
+    '''
+    pass
+
+    sta_max = meteva.base.max_of_sta(sta,used_coords=used_coords,span = 24)
+    sta_max_last_time = meteva.base.sele_by_para(sta_max,ob_hour=start_hour_of_one_day)
+    if len(sta_max_last_time.index) ==0:
+        print("没有最大值观测时间和"+str(start_hour_of_one_day).zfill(2)+"时对应的数据样本")
+    sta_max_list = [sta_max_last_time]
+    for dh in range(1,24):
+        sta_max_m = copy.deepcopy(sta_max_last_time)
+        if used_coords =="time":
+            sta_max_m["time"] -= datetime.timedelta(hours=dh)
+        elif used_coords=="dtime":
+            sta_max_m["dtime"] -= dh
+        else:
+            print("参数used_coords 只支持 time 和dtime两个选项")
+            return
+        sta_max_list.append(sta_max_m)
+    sta_max_all = meteva.base.concat(sta_max_list)
+    sta_minus = meteva.base.minus_on_level_time_dtime_id(sta,sta_max_all,how="inner")
+    values = sta_minus.values[:,6:]
+    values[values==0] = 1
+    values[values<0] = 0
+    sta_minus.iloc[:, 6:] =values[:,:]
+    if not keep_zeros:
+        sta0 = copy.deepcopy(sta)
+        values = sta0.values[:, 6:]
+        values[values != 0] = 1
+        sta0.iloc[:,6:] = values
+        sta_minus = meteva.base.mutiply_on_level_time_dtime_id(sta_minus,sta0,how="inner")
+
+    return sta_minus
+
+
+
+def is_min_of_oneday(sta,used_coords = "time",start_hour_of_one_day = 0):
+    '''
+    判断一个数据样本是否为一天中的最小值，是则记为1，否则记为0
+    :param sta:
+    :param used_coords:
+    :param start_hour_of_one_day:
+    :return:
+    '''
+    pass
+
+    sta_min = meteva.base.min_of_sta(sta,used_coords=used_coords,span = 24)
+    sta_min_last_time = meteva.base.sele_by_para(sta_min,ob_hour=start_hour_of_one_day)
+    if len(sta_min_last_time.index) ==0:
+        print("没有最大值观测时间和"+str(start_hour_of_one_day).zfill(2)+"时对应的数据样本")
+        return
+    sta_min_list = [sta_min_last_time]
+    for dh in range(1,24):
+        sta_min_m = copy.deepcopy(sta_min_last_time)
+        if used_coords =="time":
+            sta_min_m["time"] -= datetime.timedelta(hours=dh)
+        elif used_coords=="dtime":
+            sta_min_m["dtime"] -= dh
+        else:
+            print("参数used_coords 只支持 time 和dtime两个选项")
+            return
+        sta_min_list.append(sta_min_m)
+    sta_min_all = meteva.base.concat(sta_min_list)
+    sta_minus = meteva.base.minus_on_level_time_dtime_id(sta_min_all,sta,how="inner")
+    values = sta_minus.values[:,6:]
+    values[values==0] = 1
+    values[values<0] = 0
+    sta_minus.iloc[:, 6:] =values[:,:]
+
+
+    return sta_minus
