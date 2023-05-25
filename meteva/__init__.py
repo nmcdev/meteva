d6a28907b000b6495fb234db15dab149db5d75e8
liucouhua <liucouhua@163.com>
2023-05-25 10:11:03 +0800

    版本更新

--- meteva/__init__.py
+++ meteva/__init__.py
@@ -1,7 +1,7 @@
 #python setup.py sdist bdist_wheel
 #twine upload dist/*
 __author__ = "The R & D Center for Weather Forecasting Technology in NMC, CMA"
-__version__ = '1.6.5'
+__version__ = '1.7.5'
 
 from . import base
 from . import method
