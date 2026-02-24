05d59f585791ffcc6c131d91a5140bd590fa0823
liucouhua <liucouhua@163.com>
2026-02-24 15:25:48 +0800

    只在读取分布式数据时加载probobuf
    probobuf 是读取MICPAS分布式数据所需的模块，但probobuf升级后出现了加载错误，导致MetEva不可使用，为了避免该问题，将probobuf的加载只在读取分布式数据时执行

--- meteva/base/io/DataBlock_pb2.py
+++ meteva/base/io/DataBlock_pb2.py
@@ -3,18 +3,25 @@
 # source: DataBlock.proto
 
 import sys
+#import os
 _b=sys.version_info[0]<3 and (lambda x:x) or (lambda x:x.encode('latin1'))
+#import google.protobuf
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf import descriptor_pb2
+#from packaging import version
+
+
+# parsed_version = version.parse(google.protobuf.__version__)
+# target_version = version.parse("3.20.0")
+# if parsed_version >= target_version:
+
+
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
-
-
-
 
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='DataBlock.proto',
