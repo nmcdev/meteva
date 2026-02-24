130db9f123105544933bde43533aa5d4b4661b18
liucouhua <liucouhua@163.com>
2026-02-24 16:41:27 +0800

    Update identify.py

--- meteva/method/weather_system/identify.py
+++ meteva/method/weather_system/identify.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import json
 import traceback
@@ -68,6 +69,10 @@ def high_low_center(grd,output_dir_root,resolution = "low",smooth_times = 0,min_
             }
     para_json = json.dumps(para)
 
+    file1 = open(r"H:\task\develop\java\sysIdentify\para_json.txt","w")
+    file1.write(para_json)
+    file1.close()
+
     str_json = java_class_func(meteva.base.ws_jar_path,"Jpype","ws",None,para_json)
     if str_json !="null" and len(str_json)>0:
         graphy = json.loads(str_json)
@@ -102,6 +107,36 @@ def vortex(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 10
         return None
 
 
+def anti_vortex(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 500):
+    grid0 = meteva.base.get_grid_of_data(grd)
+    data = grd.values.flatten().tolist()
+    height_oy = meteva.base.read_griddata_from_nc(meteva.base.height_oy)
+    grid_h = meteva.base.get_grid_of_data(height_oy)
+    h_data = height_oy.values.flatten().tolist()
+    para = {"type":"anti_vortex",
+            "smooth_times":smooth_times,"min_size":min_size,"resolution":resolution,
+            "level" : int(grid0.levels[0]),"time" : grid0.stime_str[0:10],"dtime" : int(grid0.dtimes[0]),
+            "nlon":grid0.nlon,"nlat":grid0.nlat,"startlon":float(grid0.slon),"startlat":float(grid0.slat),"dlon":float(grid0.dlon),"dlat":float(grid0.dlat),
+            "data":data,"output_dir_root":output_dir_root,
+            "h_nlon": grid_h.nlon, "h_nlat": grid_h.nlat,
+            "h_slon": grid_h.slon, "h_slat": grid_h.slat,
+            "h_dlon": grid_h.dlon, "h_dlat": grid_h.dlat,
+            "h_data": h_data
+            }
+    para_json = json.dumps(para)
+
+    # file1 = open(r"H:\task\develop\java\sysIdentify\para_json.txt","w")
+    # file1.write(para_json)
+    # file1.close()
+
+
+    str_json = java_class_func(meteva.base.ws_jar_path,"Jpype","ws",None,para_json)
+    if str_json !="null" and len(str_json)>0:
+        graphy = json.loads(str_json)
+        return graphy
+    else:
+        return None
+
 def trough(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 100):
     grid0 = meteva.base.get_grid_of_data(grd)
     data = grd.values.flatten().tolist()
@@ -120,6 +155,10 @@ def trough(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 10
             "h_dlon": grid_h.dlon, "h_dlat": grid_h.dlat,
             "h_data":h_data}
     para_json = json.dumps(para)
+
+    file1 = open(r"H:\task\develop\java\sysIdentify\para_json.txt","w")
+    file1.write(para_json)
+    file1.close()
 
     str_json = java_class_func(meteva.base.ws_jar_path,"Jpype","ws",None,para_json)
     if str_json !="null" and len(str_json)>0:
@@ -178,6 +217,7 @@ def convergence_line(grd,output_dir_root,resolution = "low",smooth_times = 0,min
     else:
         return None
 
+
 def shear(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 100):
     grid0 =  meteva.base.get_grid_of_data(grd)
     data = grd.values.flatten().tolist()
@@ -195,6 +235,10 @@ def shear(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 100
             "h_data": h_data
             }
     para_json = json.dumps(para)
+
+    file1 = open(r"H:\task\develop\java\sysIdentify\para_json.txt","w")
+    file1.write(para_json)
+    file1.close()
 
     str_json = java_class_func(meteva.base.ws_jar_path,"Jpype","ws",None,para_json)
     if str_json !="null" and len(str_json)>0:
@@ -338,13 +382,161 @@ def tran_graphy_to_df(graphy):
     else:
         return center,None
 
+def cold_front(grd,output_dir_root,smooth_times = 0,min_size = 500):
+    grid0 =  meteva.base.get_grid_of_data(grd)
+    data = grd.values.flatten().tolist()
+    height_oy = meteva.base.read_griddata_from_nc(meteva.base.height_oy)
+    grid_h = meteva.base.get_grid_of_data(height_oy)
+    h_data = height_oy.values.flatten().tolist()
+    para = {"type":"cold_front",
+            "smooth_times":smooth_times,"min_size":min_size,"resolution":"low",
+            "level" : int(grid0.levels[0]),"time" : grid0.stime_str[0:10],"dtime" : int(grid0.dtimes[0]),
+            "nlon":grid0.nlon,"nlat":grid0.nlat,"startlon":float(grid0.slon),"startlat":float(grid0.slat),"dlon":float(grid0.dlon),"dlat":float(grid0.dlat),
+            "data":data,"output_dir_root":output_dir_root,
+            "h_nlon": grid_h.nlon, "h_nlat": grid_h.nlat,
+            "h_slon": grid_h.slon, "h_slat": grid_h.slat,
+            "h_dlon": grid_h.dlon, "h_dlat": grid_h.dlat,
+            "h_data": h_data
+            }
+    para_json = json.dumps(para)
 
+    # file1 = open(r"H:\task\develop\java\sysIdentify\para_json.txt","w")
+    # file1.write(para_json)
+    # file1.close()
+
+    str_json = java_class_func(meteva.base.ws_jar_path,"Jpype","ws",None,para_json)
+    if str_json !="null" and len(str_json)>0:
+        graphy = json.loads(str_json)
+        return graphy
+    else:
+        return None
+
+
+def warm_front(grd,output_dir_root,smooth_times = 10,min_size = 300):
+    grid0 =  meteva.base.get_grid_of_data(grd)
+    data = grd.values.flatten().tolist()
+    height_oy = meteva.base.read_griddata_from_nc(meteva.base.height_oy)
+    grid_h = meteva.base.get_grid_of_data(height_oy)
+    h_data = height_oy.values.flatten().tolist()
+    para = {"type":"warm_front",
+            "smooth_times":smooth_times,"min_size":min_size,"resolution":"low",
+            "level" : int(grid0.levels[0]),"time" : grid0.stime_str[0:10],"dtime" : int(grid0.dtimes[0]),
+            "nlon":grid0.nlon,"nlat":grid0.nlat,"startlon":float(grid0.slon),"startlat":float(grid0.slat),"dlon":float(grid0.dlon),"dlat":float(grid0.dlat),
+            "data":data,"output_dir_root":output_dir_root,
+            "h_nlon": grid_h.nlon, "h_nlat": grid_h.nlat,
+            "h_slon": grid_h.slon, "h_slat": grid_h.slat,
+            "h_dlon": grid_h.dlon, "h_dlat": grid_h.dlat,
+            "h_data": h_data
+            }
+    para_json = json.dumps(para)
+
+    file1 = open(r"H:\task\develop\java\sysIdentify\para_json.txt","w")
+    file1.write(para_json)
+    file1.close()
+
+    str_json = java_class_func(meteva.base.ws_jar_path,"Jpype","ws",None,para_json)
+    if str_json !="null" and len(str_json)>0:
+        graphy = json.loads(str_json)
+        return graphy
+    else:
+        return None
 
 
 if __name__=="__main__":
-    jar_path = r"H:\task\develop\java\sysIdentify\out\artifacts\sysIdentify_jar\sysIdentify2023.jar"
+    jar_path = r"H:\task\develop\java\sysIdentify\out\artifacts\sysIdentify_jar\sysIdentify.jar"
     set_jar_path(jar_path)
+    # grid6 = meteva.base.grid([0, 359.75, 0.25], [-89.75, 89.75, 0.25])
+    # #grid6 = meteva.base.grid([150, 230, 0.5], [15, 65, 0.5])
+    # t850 = meteva.base.read_griddata_from_micaps4(r"H:\test_data\input\mem\ws\ECMWF\T\850\18010820.000", grid=grid6)
+    # t850.values -= meteva.base.K
+    # output_dir_root = r"H:\test_data\output\method\ws"
+    # graphy_wf = warm_front(t850, output_dir_root, smooth_times=30, min_size=500)
+
+    # axs = meb.creat_axs(1, map_extend=grid6, ncol=1, add_minmap=False, wspace=1)
+    # t850_smooth = meb.smooth(t850, 5)
+    # meb.add_contourf(axs[0], t850_smooth, cmap=meb.cmaps.temp_2m)
+    # meb.add_warm_fronts(axs[0], graphy_wf, linewidth=1)  # 绘制暖锋
+
+
     output_dir_root = r"H:\test_data\output\method\ws"
-    h500 = meteva.base.read_griddata_from_micaps4(r"H:\test_data\input\mem\ws\ECMWF\H\500\22062600.012",
-                                          level=500, time="2022062600", dtime=12)
-    graphy_trough = trough(h500, output_dir_root, smooth_times=5)
+    #
+    #path = r"H:\task\link\fengyun\20221230.nc"
+    grid6 = meteva.base.grid([0, 359.5, 0.5], [-89.5, 89.5, 0.5])
+    #h = meteva.base.read_griddata_from_nc(path,grid = grid6,level=500,dtime = 0)
+    #trough(h,output_dir_root,smooth_times=30,min_size=500)
+    path = r"H:\task\link\fengyun\ECMWF_D1D_FOR_GLB_U_20250307000000_12_100_50000.MIC"
+    wind = meteva.base.read_gridwind_from_micaps11(path,grid = grid6,level=0,dtime=0)
+    shear(wind,output_dir_root,min_size= 500)
+    print()
+    #path = r"\task\link\fengyun\1747909521636.m4.MIC"
+    #grd = meteva.base.read_griddata_from_micaps4(path)
+    # print(grd)
+    #
+    # #meteva.base.write_griddata_to_nc(grd,r"H:\task\develop\java\OAL\test_data\hgt\500\2024022420.000.nc")
+    #
+    #high_low_center(grd,output_dir_root,resolution = "low",smooth_times = 0,min_size = 1,grade_interval = 40)
+
+
+    # path = r"O:\data\grid\ECMWF_HR\WIND\850\20220326\22032608.000.nc"
+    # #
+    # # import xarray
+    # # wind = xarray.open_dataset(path)
+    # # print(wind)
+    # wind = meteva.base.read_griddata_from_nc(path)
+    # print(wind)
+
+
+    #save_path = r"H:\test_data\input\mem\ws\ECMWF\WIND\850\22032608.000"
+    #wind = meteva.base.read_gridwind_from_micaps11(save_path,level=0,time = datetime.datetime(2022,3,26,8),dtime = 0)
+
+    #meteva.base.write_griddata_to_micaps11(wind,save_path=r"H:\test_data\input\mem\ws\ECMWF\WIND\850\22032608.000",creat_dir=True)
+
+    #graphy_anti_vortex =anti_vortex(wind,output_dir_root,smooth_times = 5)
+
+    # grid6 = meteva.base.grid([150, 230, 0.5], [0, 80, 0.5])
+    # t850 = meteva.base.read_griddata_from_micaps4(r"H:\test_data\input\mem\ws\ECMWF\T\850\18010820.000",grid = grid6)
+    # graphy_cf = warm_front(t850, output_dir_root, smooth_times=30)
+
+    # # 读取1000hPa位势高度场
+    # h1000 = meteva.base.read_griddata_from_micaps4(r"H:\test_data\input\mem\ws\ECMWF\H\1000\22062600.012",
+    #                                        level=1000, time="2022062600", dtime=12)
+    # # 调用高低压识别算法
+    # graphy_hl = meteva.method.weather_system.high_low_center(h1000, output_dir_root, smooth_times=10)
+    # print(graphy_hl.keys())  # 返回结果的字典中包含了 type, level,time,dtime 和 features等关键词
+    # save_path = r"H:\test_data\input\mem\ws\ECMWF\T\850\a.txt"
+    # grd = meteva.base.read_griddata_from_gds_file(save_path)
+    # save_path = r"H:\test_data\input\mem\ws\ECMWF\T\850\19071208.000"
+    # meteva.base.write_griddata_to_micaps4(grd,save_path)
+
+    #path = r"\\10.28.16.234\data2\AI\ECMWF\tigge\t850_201801.grib"
+    # import xarray as xr
+    # grd = xr.open_dataset(path)
+    # print(grd)
+    #grd = meteva.base.read_griddata_from_grib(path, value_name="t", dtime_dim="step", level_type="isobaricInhPa")
+    #print(grd)
+
+    # time1 = datetime.datetime(2018,1,1,8)
+    # while time1 < datetime.datetime(2018,2,1,8):
+    #     time_ut = time1 - datetime.timedelta(hours=8)
+    #     #grd1 = meteva.base.sele_by_para(grd,time=[time_ut],dtime = 0)
+    #     path = meteva.base.get_path(r"\\10.28.16.234\data2\AI\CRA40\2018\T\850\YYYYMMDDHH.nc",time_ut)
+    #     grd1 = meteva.base.read_griddata_from_nc(path)
+    #     save_path = meteva.base.get_path(r"H:\test_data\input\mem\ws\ECMWF\T\850\YYMMDDHH.000",time1)
+    #
+    #     meteva.base.write_griddata_to_micaps4(grd1,save_path=save_path)
+    #     time1 += datetime.timedelta(hours=12)
+    #
+
+
+    # time1 = datetime.datetime(2018,1,1,8)
+    # while time1 < datetime.datetime(2018,2,1,8):
+    #     time_ut = time1 - datetime.timedelta(hours=8)
+    #     #grd1 = meteva.base.sele_by_para(grd,time=[time_ut],dtime = 0)
+    #     path = meteva.base.get_path(r"\\10.28.16.234\data2\AI\CRA40\2018\MSL\YYYYMMDDHH.nc",time_ut)
+    #     grd1 = meteva.base.read_griddata_from_nc(path)
+    #     grd1.values /=100
+    #     save_path = meteva.base.get_path(r"H:\test_data\input\mem\ws\ECMWF\MSL\YYMMDDHH.000",time1)
+    #
+    #     meteva.base.write_griddata_to_micaps4(grd1,save_path=save_path,creat_dir=True)
+    #     time1 += datetime.timedelta(hours=12)
+
